be2427a1b125aaab201aa7448d4b07cf672902e0
Keith Lazuka <kelazuka@microsoft.com>
2017-12-08 13:09:52 -0800

    Prioritize the module declaration lookups

--- src/main/kotlin/org/elm/lang/core/stubs/index/ElmModulesIndex.kt
+++ src/main/kotlin/org/elm/lang/core/stubs/index/ElmModulesIndex.kt
@@ -1,5 +1,6 @@
 package org.elm.lang.core.stubs.index
 
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.IndexSink
@@ -10,6 +11,8 @@ import org.elm.lang.core.moduleLookupHack
 import org.elm.lang.core.psi.elements.ElmModuleDeclaration
 import org.elm.lang.core.stubs.ElmFileStub
 import org.elm.lang.core.stubs.ElmModuleDeclarationStub
+
+private val logger = Logger.getInstance(ElmModulesIndex::class.java)
 
 
 class ElmModulesIndex: StringStubIndexExtension<ElmModuleDeclaration>() {
@@ -40,8 +43,11 @@ class ElmModulesIndex: StringStubIndexExtension<ElmModuleDeclaration>() {
             val matches = StubIndex.getElements(KEY, key, project,
                     GlobalSearchScope.allScope(project),
                     ElmModuleDeclaration::class.java)
+                    .sortedWith(elmAppVsLibraryComparator)
 
-            // TODO [kl] prioritize the results
+            if (logger.isDebugEnabled && matches.size > 1)
+                logger.warn("multiple modules found for $moduleName")
+
             return matches.firstOrNull()
         }
 
@@ -55,8 +61,20 @@ class ElmModulesIndex: StringStubIndexExtension<ElmModuleDeclaration>() {
                     results.add(it)
                 }
             }
-            // TODO [kl] prioritize the results
-            return results
+            return results.sortedWith(elmAppVsLibraryComparator)
         }
     }
 }
+
+/*
+There should be only a single file in project scope for a given module name,
+but until we start loading the `elm-package.json` manifest file to determine
+source roots and library roots, we will need to deal with indexed Elm files
+that do not truly belong to THIS project. To mitigate the problem, we will
+order the module declarations such that the ones that live in the `elm-stuff`
+directory are at the end of the list.
+*/
+private val elmAppVsLibraryComparator =
+        compareBy<ElmModuleDeclaration> {
+            if (it.containingFile.virtualFile.path.contains("/elm-stuff/")) 1 else 0
+        }
