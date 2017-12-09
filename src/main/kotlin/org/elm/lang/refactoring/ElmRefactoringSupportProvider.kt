93984172fba60e57a3378b0c216250cdccb332b3
Keith Lazuka <kelazuka@microsoft.com>
2017-12-09 14:48:58 -0800

    Add in-place rename support

---
+++ src/main/kotlin/org/elm/lang/refactoring/ElmRefactoringSupportProvider.kt
@@ -0,0 +1,13 @@
+package org.elm.lang.refactoring
+
+import com.intellij.lang.refactoring.RefactoringSupportProvider
+import com.intellij.psi.PsiElement
+
+
+class ElmRefactoringSupportProvider: RefactoringSupportProvider() {
+
+    override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
+        // TODO [kl] eventually we will want to be more selective
+        return true
+    }
+}
