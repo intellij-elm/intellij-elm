26ce0409d95276393d57efb0df4a638121a09c0b
Keith Lazuka <kelazuka@microsoft.com>
2018-08-23 16:59:58 -0700

    improve tests

--- src/test/kotlin/org/elm/workspace/ConstraintTest.kt
+++ src/test/kotlin/org/elm/workspace/ConstraintTest.kt
@@ -6,16 +6,32 @@ import org.junit.Test
 class ConstraintTest {
 
     @Test
-    fun `can determine whether a version satisfies a constraint`() {
+    fun `can determine whether a version satisfies a half-open constraint`() {
         val c = Constraint(
                 low = v(1, 0, 0),
                 high = v(2, 0, 0),
                 lowOp = Constraint.Op.LESS_THAN_OR_EQUAL,
                 highOp = Constraint.Op.LESS_THAN
         )
+        assertFalse(c.contains(v(0, 9, 0)))
         assertTrue(c.contains(v(1, 0, 0)))
         assertTrue(c.contains(v(1, 1, 0)))
         assertFalse(c.contains(v(2, 0, 0)))
+    }
+
+    @Test
+    fun `can determine whether a version satisfies an inclusive constraint`() {
+        val c = Constraint(
+                low = v(1, 0, 0),
+                high = v(2, 0, 0),
+                lowOp = Constraint.Op.LESS_THAN_OR_EQUAL,
+                highOp = Constraint.Op.LESS_THAN_OR_EQUAL
+        )
+        assertFalse(c.contains(v(0, 9, 0)))
+        assertTrue(c.contains(v(1, 0, 0)))
+        assertTrue(c.contains(v(1, 1, 0)))
+        assertTrue(c.contains(v(2, 0, 0)))
+        assertFalse(c.contains(v(2, 1, 0)))
     }
 
     @Test
