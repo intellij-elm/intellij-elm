458abc00610ce99f1595565971b39518821dd7f4
AJ Alt <ajalt@users.noreply.github.com>
2018-08-28 12:44:59 -0700

    Exclude imports from "Find Usages" results (#71)

---
+++ src/main/kotlin/org/elm/ide/usages/ElmImportFilteringRule.kt
@@ -0,0 +1,20 @@
+package org.elm.ide.usages
+
+import com.intellij.usages.Usage
+import com.intellij.usages.UsageTarget
+import com.intellij.usages.rules.ImportFilteringRule
+import com.intellij.usages.rules.PsiElementUsage
+import org.elm.lang.core.psi.ElmFile
+import org.elm.lang.core.psi.elements.ElmImportClause
+import org.elm.lang.core.psi.parentOfType
+
+/**
+ * A filtering rule that allows you exclude imports from actions such as "find usages"
+ */
+class ElmImportFilteringRule : ImportFilteringRule() {
+    override fun isVisible(usage: Usage, targets: Array<UsageTarget>): Boolean {
+        val element = (usage as? PsiElementUsage)?.element
+        return element?.containingFile !is ElmFile
+                || element.parentOfType<ElmImportClause>() == null
+    }
+}
