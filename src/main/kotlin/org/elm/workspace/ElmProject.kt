intellij-elm/intellij-elm

commit c33d353382621bb92dc3d4f021878cc932fcc42e

Allow multiple Elm projects to co-exist in the same IntelliJ project (#92)
* Simplify the representation of exposed modules on Elm package projects
* Allow multiple Elm projects to be attached within the same workspace
* All Elm projects now have source-directories
* Load project details for each package dependency
* Add test for resolving references within an Elm project
* Use the Elm project's source-dirs to restrict import visibility
* Use the Elm project's package dependencies to restrict import visibility

src/main/kotlin/org/elm/workspace/ElmProject.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.JsonNodeType
+import com.intellij.openapi.vfs.LocalFileSystem
 import org.elm.workspace.ElmToolchain.Companion.ELM_LEGACY_JSON
 import java.io.InputStream
 import java.nio.file.Path
@@ -21,11 +22,13 @@ private val objectMapper = ObjectMapper()
  * @param manifestPath The location of the manifest file (e.g. `elm.json`). Uniquely identifies a project.
  * @param dependencies Additional Elm packages that this project depends on
  * @param testDependencies Additional Elm packages that this project's **tests** depends on
+ * @param sourceDirectories The paths to one-or-more directories containing Elm source files belonging to this project.
  */
 sealed class ElmProject(
         val manifestPath: Path,
-        val dependencies: List<ElmPackageRef>,
-        val testDependencies: List<ElmPackageRef>
+        val dependencies: List<ElmPackageProject>,
+        val testDependencies: List<ElmPackageProject>,
+        val sourceDirectories: List<Path>
 ) {
 
     /**
@@ -49,17 +52,23 @@ sealed class ElmProject(
      * Returns all packages which this project depends on, whether it be for normal,
      * production code or for tests.
      */
-    val allResolvedDependencies: Sequence<ElmPackageRef>
+    val allResolvedDependencies: Sequence<ElmPackageProject>
         get() = sequenceOf(dependencies, testDependencies).flatten()
 
 
     companion object {
+
+        fun parse(manifestPath: Path, toolchain: ElmToolchain): ElmProject {
+            val inputStream = LocalFileSystem.getInstance().refreshAndFindFileByPath(manifestPath.toString())?.inputStream
+                    ?: throw ProjectLoadException("Could not find file $manifestPath")
+            return parse(inputStream, manifestPath, toolchain)
+        }
+
         /**
          * Attempts to parse an `elm.json` file.
          *
          * @throws ProjectLoadException if the JSON cannot be parsed
          */
-        @Throws(ProjectLoadException::class)
         fun parse(inputStream: InputStream, manifestPath: Path, toolchain: ElmToolchain): ElmProject {
             if (manifestPath.endsWith(ELM_LEGACY_JSON)) {
                 // Handle legacy Elm 0.18 package. We don't need to model the dependencies
@@ -90,8 +99,8 @@ sealed class ElmProject(
                     ElmApplicationProject(
                             manifestPath = manifestPath,
                             elmVersion = dto.elmVersion,
-                            dependencies = dto.dependencies.toPackageRefs(toolchain),
-                            testDependencies = dto.testDependencies.toPackageRefs(toolchain),
+                            dependencies = dto.dependencies.depsToPackages(toolchain),
+                            testDependencies = dto.testDependencies.depsToPackages(toolchain),
                             sourceDirectories = dto.sourceDirectories
                     )
                 }
@@ -111,6 +120,7 @@ sealed class ElmProject(
                             elmVersion = dto.elmVersion,
                             dependencies = dto.dependencies.constraintDepsToPackages(toolchain),
                             testDependencies = dto.testDependencies.constraintDepsToPackages(toolchain),
+                            sourceDirectories = listOf(Paths.get("src")),
                             name = dto.name,
                             version = dto.version,
                             exposedModules = dto.exposedModulesNode.toExposedModuleMap())
@@ -129,10 +139,10 @@ sealed class ElmProject(
 class ElmApplicationProject(
         manifestPath: Path,
         val elmVersion: Version,
-        dependencies: List<ElmPackageRef>,
-        testDependencies: List<ElmPackageRef>,
-        val sourceDirectories: List<String>
-) : ElmProject(manifestPath, dependencies, testDependencies)
+        dependencies: List<ElmPackageProject>,
+        testDependencies: List<ElmPackageProject>,
+        sourceDirectories: List<Path>
+) : ElmProject(manifestPath, dependencies, testDependencies, sourceDirectories)
 
 
 /**
@@ -141,67 +151,43 @@ class ElmApplicationProject(
 class ElmPackageProject(
         manifestPath: Path,
         val elmVersion: Constraint,
-        dependencies: List<ElmPackageRef>,
-        testDependencies: List<ElmPackageRef>,
+        dependencies: List<ElmPackageProject>,
+        testDependencies: List<ElmPackageProject>,
+        sourceDirectories: List<Path>,
         val name: String,
         val version: Version,
-        /** Map from label to one-or-more module names. The label can be the empty string. */
-        val exposedModules: Map<String, List<String>>
-) : ElmProject(manifestPath, dependencies, testDependencies)
+        val exposedModules: List<String>
+) : ElmProject(manifestPath, dependencies, testDependencies, sourceDirectories)
 
 
-/**
- * A dependency reference to an Elm package
- */
-class ElmPackageRef(
-        val rootPath: Path?,
-        val name: String,
-        val version: Version
-)
-
-
-private fun ExactDependenciesDTO.toPackageRefs(toolchain: ElmToolchain) =
+private fun ExactDependenciesDTO.depsToPackages(toolchain: ElmToolchain) =
         direct.depsToPackages(toolchain) + indirect.depsToPackages(toolchain)
 
 
 private fun Map<String, Version>.depsToPackages(toolchain: ElmToolchain) =
         map { (name, version) ->
-            ElmPackageRef(
-                    rootPath = toolchain.packageVersionDir(name, version),
-                    name = name,
-                    version = version)
+            loadPackage(toolchain, name, version)
         }
 
 private fun Map<String, Constraint>.constraintDepsToPackages(toolchain: ElmToolchain) =
         map { (name, constraint) ->
-            val useVersion = toolchain.availableVersionsForPackage(name)
+            val version = toolchain.availableVersionsForPackage(name)
                     .filter { constraint.contains(it) }
                     .sorted()
                     .firstOrNull()
+                    ?: throw ProjectLoadException("Could not load $name ($constraint)")
 
-            ElmPackageRef(
-                    rootPath = useVersion?.let { toolchain.packageVersionDir(name, it) },
-                    name = name,
-                    version = useVersion ?: Version.UNKNOWN)
+            loadPackage(toolchain, name, version)
         }
 
+fun loadPackage(toolchain: ElmToolchain, name: String, version: Version): ElmPackageProject {
+    val manifestPath = toolchain.findPackageManifest(name, version)
+            ?: throw ProjectLoadException("Could not load $name ($version): manifest not found")
+    // TODO [kl] guard against circular dependencies
+    val elmProject = ElmProject.parse(manifestPath, toolchain) as? ElmPackageProject
+            ?: throw ProjectLoadException("Could not load $name ($version): expected a package!")
 
-private fun JsonNode.toExposedModuleMap(): Map<String, List<String>> {
-    // normalize the 2 exposed-modules formats into a single format
-    return when (this.nodeType) {
-        JsonNodeType.ARRAY -> {
-            val moduleNames = this.elements().asSequence().map { it.textValue() }.toList()
-            mapOf("" to moduleNames)
-        }
-        JsonNodeType.OBJECT -> {
-            this.fields().asSequence().map { (label, nameNodes) ->
-                label to nameNodes.asSequence().map { it.textValue() }.toList()
-            }.toMap()
-        }
-        else -> {
-            throw RuntimeException("exposed-modules JSON must be either an array or an object")
-        }
-    }
+    return elmProject
 }
 
 
@@ -226,7 +212,7 @@ private interface ElmProjectDTO
 
 private class ElmApplicationProjectDTO(
         @JsonProperty("elm-version") val elmVersion: Version,
-        @JsonProperty("source-directories") val sourceDirectories: List<String>,
+        @JsonProperty("source-directories") val sourceDirectories: List<Path>,
         @JsonProperty("dependencies") val dependencies: ExactDependenciesDTO,
         @JsonProperty("test-dependencies") val testDependencies: ExactDependenciesDTO
 ) : ElmProjectDTO
@@ -244,9 +230,26 @@ private class ElmPackageProjectDTO(
         @JsonProperty("test-dependencies") val testDependencies: Map<String, Constraint>,
         @JsonProperty("name") val name: String,
         @JsonProperty("version") val version: Version,
-        @JsonProperty("exposed-modules") val exposedModulesNode: JsonNode // either List<String>
-        // or Map<String, List<String>>
-        // where the map's keys are labels
+        @JsonProperty("exposed-modules") val exposedModulesNode: JsonNode
 ) : ElmProjectDTO
 
 
+private fun JsonNode.toExposedModuleMap(): List<String> {
+    // Normalize the 2 exposed-modules formats into a single format.
+    // format 1: a list of strings, where each string is the name of an exposed module
+    // format 2: a map where the keys are categories and the values are the names of the modules
+    //           exposed in that category. We discard the categories because they are not useful.
+    return when (this.nodeType) {
+        JsonNodeType.ARRAY -> {
+            this.elements().asSequence().map { it.textValue() }.toList()
+        }
+        JsonNodeType.OBJECT -> {
+            this.fields().asSequence().flatMap { (_, nameNodes) ->
+                nameNodes.asSequence().map { it.textValue() }
+            }.toList()
+        }
+        else -> {
+            throw RuntimeException("exposed-modules JSON must be either an array or an object")
+        }
+    }
+}

src/main/kotlin/org/elm/lang/core/completion/ElmQualifiableRefSuggestor.kt
@@ -9,7 +9,7 @@ import org.elm.lang.core.resolve.scope.ExpressionScope
 import org.elm.lang.core.resolve.scope.GlobalScope
 import org.elm.lang.core.resolve.scope.ImportScope
 import org.elm.lang.core.resolve.scope.ModuleScope
-import org.elm.lang.core.stubs.index.ElmModulesIndex
+import org.elm.lang.core.stubs.index.ElmModules
 
 
 /**
@@ -75,7 +75,7 @@ object ElmQualifiableRefSuggestor : Suggestor {
     }
 
     private fun suggestQualifiers(qualifierPrefix: String, file: ElmFile, result: CompletionResultSet) {
-        ElmModulesIndex.getAll(file.project)
+        ElmModules.getAll(file.project, file.elmProject)
                 .filter { it.name.startsWith(qualifierPrefix) && it.name != qualifierPrefix }
                 .map { it.name.removePrefix("$qualifierPrefix.").substringBefore('.') }
                 .forEach { result.add(it) }

src/main/kotlin/org/elm/lang/core/psi/ElmFile.kt
@@ -11,6 +11,8 @@ import com.intellij.psi.stubs.StubElement
 import org.elm.lang.core.ElmFileType
 import org.elm.lang.core.ElmLanguage
 import org.elm.lang.core.stubs.*
+import org.elm.workspace.ElmProject
+import org.elm.workspace.elmWorkspace
 
 
 class ElmFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, ElmLanguage) {
@@ -39,6 +41,12 @@ class ElmFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, ElmLan
         val nameWithExtension = if (name.endsWith(".elm")) name else "$name.elm"
         return super.setName(nameWithExtension)
     }
+
+    val elmProject: ElmProject?
+        // Must use [originalFile] because during code completion the direct [virtualFile] is an
+        // in-memory copy of the real file. See:
+        // https://intellij-support.jetbrains.com/hc/en-us/community/posts/115000108704/comments/115000123710
+        get() = project.elmWorkspace.findProjectForFile(originalFile.virtualFile)
 
     fun getModuleDecl() =
             getStubOrPsiChild(ElmModuleDeclarationStub.Type)

src/main/kotlin/org/elm/lang/core/psi/ElmPsiElementImpl.kt
@@ -8,6 +8,8 @@ import com.intellij.psi.StubBasedPsiElement
 import com.intellij.psi.stubs.IStubElementType
 import com.intellij.psi.stubs.StubElement
 import org.elm.lang.core.resolve.reference.ElmReference
+import org.elm.workspace.ElmProject
+import org.elm.workspace.elmWorkspace
 
 
 /**
@@ -18,6 +20,14 @@ interface ElmPsiElement : PsiElement {
      * Get the file containing this element as an [ElmFile]
      */
     val elmFile: ElmFile
+
+    /**
+     * Get the Elm project which this element's file belongs to.
+     *
+     * Returns null if the containing Elm project's manifest (`elm.json`) has not
+     * yet been attached to the workspace.
+     */
+    val elmProject: ElmProject?
 }
 
 /**
@@ -27,6 +37,9 @@ abstract class ElmPsiElementImpl(node: ASTNode) : ASTWrapperPsiElement(node), El
 
     override val elmFile: ElmFile
         get() = containingFile as ElmFile
+
+    override val elmProject: ElmProject?
+        get() = elmFile.elmProject
 
     // Make the type-system happy by using our reference interface instead of PsiReference
     override fun getReferences(): Array<ElmReference> {
@@ -51,6 +64,8 @@ abstract class ElmStubbedElement<StubT : StubElement<*>>
     override val elmFile: ElmFile
         get() = containingFile as ElmFile
 
+    override val elmProject: ElmProject?
+        get() = project.elmWorkspace.findProjectForFile(elmFile.virtualFile)
 
     // Make the type-system happy by using our reference interface instead of PsiReference
     override fun getReferences(): Array<ElmReference> {

src/main/kotlin/org/elm/lang/core/psi/elements/ElmImportClause.kt
@@ -1,13 +1,15 @@
 package org.elm.lang.core.psi.elements
 
 import com.intellij.lang.ASTNode
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.psi.PsiElement
 import org.elm.lang.core.psi.ElmNamedElement
 import org.elm.lang.core.psi.ElmPsiElementImpl
 import org.elm.lang.core.resolve.ElmReferenceElement
 import org.elm.lang.core.resolve.reference.ElmReferenceCached
-import org.elm.lang.core.stubs.index.ElmModulesIndex
+import org.elm.lang.core.stubs.index.ElmModules
 
+private val log = logger<ElmImportClause>()
 
 /**
  * An import declaration at the top of the module.
@@ -44,11 +46,10 @@ class ElmImportClause(node: ASTNode) : ElmPsiElementImpl(node), ElmReferenceElem
     override fun getReference() =
             object : ElmReferenceCached<ElmImportClause>(this) {
 
-                override fun resolveInner(): ElmNamedElement? {
-                    return getVariants().find { it.name == element.referenceName }
-                }
+                override fun resolveInner(): ElmNamedElement? =
+                        getVariants().find { it.name == element.referenceName }
 
                 override fun getVariants(): Array<ElmNamedElement> =
-                        ElmModulesIndex.getAll(element.project).toTypedArray()
+                        ElmModules.getAll(project, element.elmProject).toTypedArray()
             }
 }

src/main/kotlin/org/elm/lang/core/resolve/reference/QualifiedModuleNameReference.kt
@@ -11,6 +11,7 @@ import org.elm.lang.core.psi.offsetIn
 import org.elm.lang.core.resolve.ElmReferenceElement
 import org.elm.lang.core.resolve.scope.GlobalScope
 import org.elm.lang.core.resolve.scope.ModuleScope
+import org.elm.lang.core.stubs.index.ElmModules
 import org.elm.lang.core.stubs.index.ElmModulesIndex
 
 /**
@@ -38,7 +39,7 @@ class QualifiedModuleNameReference<T : ElmReferenceElement>(
                         .let { ElmModulesIndex.getAll(it, element.project) }
 
         val implicitDecls =
-                ElmModulesIndex.getAll(GlobalScope.defaultImports, element.project)
+                ElmModules.getAll(GlobalScope.defaultImports, element.project, element.elmProject)
                         .filter { it.elmFile.isCore() }
 
         val aliasDecls = ModuleScope(element.elmFile).getAliasDecls() as List<ElmNamedElement>

src/main/kotlin/org/elm/lang/core/resolve/scope/GlobalScope.kt
@@ -2,14 +2,18 @@ package org.elm.lang.core.resolve.scope
 
 import com.intellij.openapi.project.Project
 import org.elm.lang.core.psi.ElmNamedElement
-import org.elm.lang.core.stubs.index.ElmModulesIndex
+import org.elm.lang.core.stubs.index.ElmModules
+import org.elm.workspace.ElmProject
 
 
 /**
  * The subset of implicitly exposed values, types and constructors provided by Elm's
  * standard library ("Core").
  */
-class GlobalScope(val project: Project) {
+// TODO [kl] eventually ElmProject should be non-null, but we need to straighten out
+//           some things with the integration tests and legacy Elm 0.18 projects before
+//           we can be more restrictive here.
+class GlobalScope(val project: Project, val elmProject: ElmProject?) {
 
     companion object {
         /**
@@ -47,7 +51,7 @@ class GlobalScope(val project: Project) {
 
     fun getVisibleValues(): List<ElmNamedElement> {
         fun helper(moduleName: String) =
-                ElmModulesIndex.get(moduleName, project)
+                ElmModules.get(moduleName, project, elmProject)
                         ?.let { ModuleScope(it.elmFile).getDeclaredValues() }
                         ?: emptyList()
 
@@ -64,7 +68,7 @@ class GlobalScope(val project: Project) {
 
     fun getVisibleTypes(): List<ElmNamedElement> {
         fun helper(moduleName: String) =
-                ElmModulesIndex.get(moduleName, project)
+                ElmModules.get(moduleName, project, elmProject)
                         ?.let { ModuleScope(it.elmFile).getDeclaredTypes() }
                         ?: emptyList()
 
@@ -83,7 +87,7 @@ class GlobalScope(val project: Project) {
 
     fun getVisibleConstructors(): List<ElmNamedElement> {
         fun helper(moduleName: String) =
-                ElmModulesIndex.get(moduleName, project)
+                ElmModules.get(moduleName, project, elmProject)
                         ?.let { ModuleScope(it.elmFile).getDeclaredConstructors() }
                         ?: emptyList()
 

src/main/kotlin/org/elm/lang/core/resolve/scope/ImportScope.kt
@@ -5,7 +5,7 @@ import org.elm.lang.core.psi.ElmNamedElement
 import org.elm.lang.core.psi.elements.ElmImportClause
 import org.elm.lang.core.psi.elements.ElmTypeAliasDeclaration
 import org.elm.lang.core.psi.elements.ElmTypeDeclaration
-import org.elm.lang.core.stubs.index.ElmModulesIndex
+import org.elm.lang.core.stubs.index.ElmModules
 
 
 /**
@@ -23,7 +23,7 @@ class ImportScope(val elmFile: ElmFile) {
          */
         fun fromImportDecl(importDecl: ElmImportClause): ImportScope? {
             val moduleName = importDecl.moduleQID.text
-            return ElmModulesIndex.get(moduleName, importDecl.project)
+            return ElmModules.get(moduleName, importDecl.project, importDecl.elmProject)
                     ?.let { ImportScope(it.elmFile) }
         }
 
@@ -34,7 +34,7 @@ class ImportScope(val elmFile: ElmFile) {
          */
         fun fromQualifierPrefixInModule(qualifierPrefix: String, elmFile: ElmFile): List<ImportScope> {
             // handle implicit imports from Core
-            val implicitScopes = ElmModulesIndex.getAll(listOf(qualifierPrefix), elmFile.project)
+            val implicitScopes = ElmModules.getAll(listOf(qualifierPrefix), elmFile.project, elmFile.elmProject)
                     .filter { it.elmFile.isCore() && qualifierPrefix in GlobalScope.defaultImports }
                     .map { ImportScope(it.elmFile) }
 

commit ab8510029b44ac2e85e6c72006ead5b2b72bf04e

Fix references to Cmd and Sub
This fixes a regression in #92 that caused implicit references to `Cmd` and `Sub` not to resolve. These two values are special-cased in `moduleLookupHack` when creating the module definition, but not the dependency exposed modules, so `ElmProject.exposes` was returning false because `exposedModules` contained `Platform.Cmd`, but the module declaration name was just `Cmd`

I added the hack to the `exposedModules`, but I don't know if that's the correct fix. Maybe the hack is no longer necessary?

src/main/kotlin/org/elm/workspace/ElmProject.kt
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.JsonNodeType
 import com.intellij.openapi.vfs.LocalFileSystem
+import org.elm.lang.core.moduleLookupHack
 import org.elm.workspace.ElmToolchain.Companion.ELM_LEGACY_JSON
 import java.io.InputStream
 import java.nio.file.Path
@@ -241,11 +242,11 @@ private fun JsonNode.toExposedModuleMap(): List<String> {
     //           exposed in that category. We discard the categories because they are not useful.
     return when (this.nodeType) {
         JsonNodeType.ARRAY -> {
-            this.elements().asSequence().map { it.textValue() }.toList()
+            this.elements().asSequence().map { moduleLookupHack(it.textValue()) }.toList()
         }
         JsonNodeType.OBJECT -> {
             this.fields().asSequence().flatMap { (_, nameNodes) ->
-                nameNodes.asSequence().map { it.textValue() }
+                nameNodes.asSequence().map { moduleLookupHack(it.textValue()) }
             }.toList()
         }
         else -> {