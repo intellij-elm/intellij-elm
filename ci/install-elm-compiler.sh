bca7fa7cbf46e4a4e2e20578499874dda87131f9
Keith Lazuka <kelazuka@microsoft.com>
2018-10-27 11:05:38 -0700

    Maybe we don't need sudo

--- ci/install-elm-compiler.sh
+++ ci/install-elm-compiler.sh
@@ -2,5 +2,5 @@
 
 wget "https://github.com/elm/compiler/releases/download/0.19.0/binaries-for-linux.tar.gz"
 tar zxvf binaries-for-linux.tar.gz
-sudo mv elm /usr/local/bin/
+mv elm /usr/local/bin/
 elm --version
