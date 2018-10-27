763e5f04a95b918295613388373197c322f6c2c7
Keith Lazuka <kelazuka@microsoft.com>
2018-10-27 11:20:50 -0700

    Use package managers to install Elm on all platforms

--- ci/install-elm-compiler.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget "https://github.com/elm/compiler/releases/download/0.19.0/binaries-for-linux.tar.gz"
-tar zxvf binaries-for-linux.tar.gz
-mv elm /usr/local/bin/
-elm --version
