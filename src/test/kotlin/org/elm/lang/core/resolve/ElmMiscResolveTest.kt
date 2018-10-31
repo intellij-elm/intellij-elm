7e6a2a89a32a6c87f945c73890adbbadbe605a38
Keith Lazuka <klazuka@gmail.com>
2018-10-31 11:09:48 -0700

    Fix ref resolve for case-of branches and anonymous functions (#108)
    Both ElmCaseOfBranch and ElmAnonymousFunction were being 
    too lenient when providing the names which it introduces into the 
    lexical scope.
    
    We were descending down into both the patterns on the left-side of
    the branch (good) AND the expression on the right-hand side (bad!)

--- src/test/kotlin/org/elm/lang/core/resolve/ElmMiscResolveTest.kt
+++ src/test/kotlin/org/elm/lang/core/resolve/ElmMiscResolveTest.kt
@@ -4,11 +4,11 @@ package org.elm.lang.core.resolve
 /**
  * Miscellaneous tests related to the reference-resolve system
  */
-class ElmMiscResolveTest: ElmResolveTestBase() {
+class ElmMiscResolveTest : ElmResolveTestBase() {
 
 
     fun `test top-level value ref`() = checkByCode(
-"""
+            """
 magicNumber = 42
 --X
 
@@ -17,8 +17,11 @@ f = magicNumber + 1
 """)
 
 
+    // LET-IN EXPRESSIONS
+
+
     fun `test simple value declared by let-in`() = checkByCode(
-"""
+            """
 f x =
     let y = 42
       --X
@@ -28,7 +31,7 @@ f x =
 
 
     fun `test let-in should honor lexical scope in body expr`() = checkByCode(
-"""
+            """
 foo =
     let
         bar y = 0
@@ -38,7 +41,7 @@ foo =
 
 
     fun `test let-in should honor lexical scope in sibling decl`() = checkByCode(
-"""
+            """
 foo =
     let
         bar y = 0
@@ -49,35 +52,54 @@ foo =
 """)
 
 
+    // LAMBDAS (ANONYMOUS FUNCTIONS)
+
+
     fun `test lambda parameter ref`() = checkByCode(
-"""
+            """
 f = \x -> x
    --X  --^
 """)
 
+    fun `test lambda parameter nested`() = checkByCode(
+            """
+f = \x -> (\() -> x)
+   --X          --^
+""")
+
+
+    fun `test lambda parameter nested and should not resolve`() = checkByCode(
+            """
+f = \() -> x (\x -> ())
+         --^unresolved
+""")
+
 
     fun `test lambda parameter destructured record field ref`() = checkByCode(
-"""
+            """
 f = \{x} -> x
     --X   --^
 """)
 
 
     fun `test lambda parameter destructured tuple ref`() = checkByCode(
-"""
+            """
 f = \(x,y) -> x
     --X     --^
 """)
 
     fun `test lambda parameter destructured with alias`() = checkByCode(
-"""
+            """
 f = \((x,y) as point) -> point
                --X       --^
 """)
 
 
+    // PORTS
+
+
     fun `test port ref`() = checkByCode(
-"""
+            """
 port module Ports exposing (..)
 port foo : String -> Cmd msg
      --X
--- src/test/kotlin/org/elm/lang/core/resolve/ElmPatternResolveTest.kt
+++ src/test/kotlin/org/elm/lang/core/resolve/ElmPatternResolveTest.kt
@@ -4,11 +4,14 @@ package org.elm.lang.core.resolve
 /**
  * Tests related to pattern matching and destructuring
  */
-class ElmPatternResolveTest: ElmResolveTestBase() {
+class ElmPatternResolveTest : ElmResolveTestBase() {
+
+
+    // CASE-OF EXPRESSIONS AND PATTERNS
 
 
     fun `test case-of pattern wildcard`() = checkByCode(
-"""
+            """
 f x =
     case x of
         0 -> 0
@@ -19,7 +22,7 @@ f x =
 
 
     fun `test case-of pattern union type constructor`() = checkByCode(
-"""
+            """
 f x =
     case x of
         Nothing -> 0
@@ -29,22 +32,64 @@ f x =
     )
 
 
-    fun `test function parameter record destructuring`() = checkByCode(
+    fun `test case-of that should not resolve`() = checkByCode(
+            """
+f x =
+    case () of
+        Just foo -> ()
+        _ -> foo
+             --^unresolved
 """
+    )
+
+
+    fun `test nested case-of`() = checkByCode(
+            """
+f x =
+    case () of
+        _ ->
+            case () of
+                _ -> ()
+                Just foo -> foo
+                     --X    --^
+"""
+    )
+
+
+    // see bug https://github.com/klazuka/intellij-elm/issues/106
+    fun `test nested case-of that should not resolve`() = checkByCode(
+            """
+f x =
+    case () of
+        _ ->
+            case () of
+                Just foo -> ()
+                _ -> foo
+                     --^unresolved
+"""
+    )
+
+
+    // PARAMETER DESTRUCTURING
+
+
+    fun `test function parameter record destructuring`() = checkByCode(
+            """
 foo { name } = name
       --X      --^
 """)
 
 
     fun `test function parameter tuple destructuring`() = checkByCode(
-"""
+            """
 foo ( x, y ) = x + y
        --X       --^
 """)
 
 
+    // TODO [drop 0.18] this becomes invalid at the top-level in 0.19
     fun `test top-level value destructuring`() = checkByCode(
-"""
+            """
 ( x, y ) = (0, 0)
    --X
 
@@ -54,7 +99,7 @@ f = y + 20
 
 
     fun `test nested function parameter destructuring`() = checkByCode(
-"""
+            """
 f =
     let
         g ( x, y ) = x + y
@@ -64,15 +109,18 @@ f =
 """)
 
 
+    // PATTERN ALIASES
+
+
     fun `test pattern alias in function decl parameter`() = checkByCode(
-"""
+            """
 foo ((x, y) as point) = point
                --X      --^
 """)
 
 
     fun `test pattern alias in let-in decl`() = checkByCode(
-"""
+            """
 f =
     let
         g ((x, y) as point) = point
@@ -83,7 +131,7 @@ f =
 
 
     fun `test pattern alias in case-of branch`() = checkByCode(
-"""
+            """
 f x =
     case x of
         ((x, y) as point) -> point
