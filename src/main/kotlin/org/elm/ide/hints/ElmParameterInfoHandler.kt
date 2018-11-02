b886dc236f17d05c361c6d66d05da2d2629d2e60
Keith Lazuka <klazuka@gmail.com>
2018-11-02 08:45:54 -0700

    Add 'parameter info' support (ctrl-p/cmd-p) (#111)

---
+++ src/main/kotlin/org/elm/ide/hints/ElmParameterInfoHandler.kt
@@ -0,0 +1,118 @@
+package org.elm.ide.hints
+
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.lang.parameterInfo.*
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import org.elm.lang.core.psi.ElmTypes.RIGHT_PARENTHESIS
+import org.elm.lang.core.psi.ElmTypes.VIRTUAL_END_DECL
+import org.elm.lang.core.psi.ancestorsStrict
+import org.elm.lang.core.psi.elementType
+import org.elm.lang.core.psi.elements.CallInfo
+import org.elm.lang.core.psi.elements.ElmFunctionCall
+import org.elm.lang.core.types.renderedText
+
+private val log = logger<ElmParameterInfoHandler>()
+
+/**
+ * Provide information about the parameters expected by a function at a call-site.
+ * In a normal programming language, this would just show the parameter names/types
+ * and highlight the parameter which the caret currently corresponds to. But that
+ * doesn't work well with ML-style languages where functions are partially applied,
+ * application order can be inverted, and where there are no delimiting parentheses
+ * around the function's arguments.
+ *
+ * So instead we will just show the function call target's type annotation.
+ */
+class ElmParameterInfoHandler : ParameterInfoHandler<PsiElement, ElmParametersDescription> {
+
+    var hintText: String = ""
+
+    override fun couldShowInLookup() = false
+
+    override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?) =
+    // TODO maybe we should implement this. I'm not sure what it does, though.
+            null
+
+    override fun findElementForParameterInfo(context: CreateParameterInfoContext): PsiElement? {
+        val caretElement = context.file.findElementAt(context.editor.caretModel.offset) ?: return null
+        val element = findFuncCall(caretElement)
+        log.debug("findElementForParameterInfo() caret on $caretElement returning $element")
+        return element
+    }
+
+    override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext): PsiElement? {
+        val caretElement = context.file.findElementAt(context.editor.caretModel.offset) ?: return null
+        return findFuncCall(caretElement)
+    }
+
+    private fun findFuncCall(caretElement: PsiElement): ElmFunctionCall? {
+        val element = when (caretElement.elementType) {
+            VIRTUAL_END_DECL, RIGHT_PARENTHESIS -> PsiTreeUtil.prevVisibleLeaf(caretElement) ?: return null
+            else -> caretElement
+        }
+
+        val ancestorsStrict = element.ancestorsStrict
+        log.debug("findFuncCall for $element (${element.text}) ancestorsStrict=${ancestorsStrict.toList()}")
+        return ancestorsStrict.filterIsInstance<ElmFunctionCall>().firstOrNull()
+    }
+
+    // receives the element as returned by findElementForParameterInfo
+    override fun showParameterInfo(element: PsiElement, context: CreateParameterInfoContext) {
+        if (element !is ElmFunctionCall) return
+
+        val paramsDescription = ElmParametersDescription.fromCall(element)
+                ?: return
+
+        // Each "item" in `itemsToShow` is a function overload set. Elm does not support function overloading,
+        // so this array will never be larger than length 1.
+        context.itemsToShow = arrayOf(paramsDescription)
+
+        context.showHint(element, element.textRange.startOffset, this)
+    }
+
+    override fun updateParameterInfo(parameterOwner: PsiElement, context: UpdateParameterInfoContext) {
+        // normally you would call context.setCurrentParameter() here, but we are not going
+        // to try to highlight the current parameter
+    }
+
+    override fun updateUI(p: ElmParametersDescription?, context: ParameterInfoUIContext) {
+        if (p == null) {
+            context.isUIComponentEnabled = false
+            return
+        }
+
+        hintText = p.presentText
+        val range = p.rangeToHighlight
+
+        context.setupUIComponentPresentation(
+                hintText,
+                range.startOffset,
+                range.endOffset,
+                !context.isUIComponentEnabled,
+                false,
+                false,
+                context.defaultParameterColor
+        )
+    }
+}
+
+class ElmParametersDescription(val callInfo: CallInfo) {
+    val presentText: String
+        get() {
+            val signature = (callInfo.parameters.map { it.ty } + callInfo.returnType)
+                    .joinToString(" → ") { it.renderedText(linkify = false, withModule = false) }
+            return "${callInfo.functionName} : $signature"
+        }
+    val rangeToHighlight: TextRange
+        get() = TextRange(0, callInfo.functionName.length)
+
+    companion object {
+        fun fromCall(functionCall: ElmFunctionCall): ElmParametersDescription? {
+            val info = functionCall.resolveCallInfo() ?: return null
+            return ElmParametersDescription(info)
+        }
+    }
+}
---
+++ src/test/kotlin/org/elm/ide/hints/ElmParameterInfoHandlerTest.kt
@@ -0,0 +1,127 @@
+/*
+The MIT License (MIT)
+
+Derived from intellij-rust
+Copyright (c) 2015 Aleksey Kladov, Evgeny Kurbatsky, Alexey Kudinkin and contributors
+Copyright (c) 2016 JetBrains
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+package org.elm.ide.hints
+
+import com.intellij.testFramework.utils.parameterInfo.MockCreateParameterInfoContext
+import com.intellij.testFramework.utils.parameterInfo.MockParameterInfoUIContext
+import com.intellij.testFramework.utils.parameterInfo.MockUpdateParameterInfoContext
+import junit.framework.AssertionFailedError
+import junit.framework.TestCase
+import org.elm.lang.ElmTestBase
+import org.intellij.lang.annotations.Language
+
+class ElmParameterInfoHandlerTest : ElmTestBase() {
+    // TODO: At some point, the type annotations on each function should no longer be necessary.
+    //       But right now the type inference system needs them.
+
+
+    // must include Elm stdlib because the type inference code must resolve types like String, Char, Int
+    override fun getProjectDescriptor() =
+            ElmWithStdlibDescriptor
+
+
+    // SOLO ARGUMENT
+
+
+    fun `test function with one arg`() {
+        checkByText("""
+f : String -> Int
+f x = 42
+main = f "foo"{-caret-}
+""", "f : String → Int")
+    }
+
+    fun `test function with one arg nested in another function call`() {
+        checkByText("""
+f : Int -> String
+f x = "blah"
+g : Char -> Int
+g x = 99
+main = f (g 'c'{-caret-})
+""", "g : Char → Int")
+    }
+
+
+    // MULTIPLE ARGUMENTS
+
+
+    fun `test function with two args, caret on first arg`() {
+        checkByText("""
+f : String -> Char -> Int
+f x y = 42
+main = f "hi"{-caret-}
+""", "f : String → Char → Int")
+    }
+
+    fun `test function with two args, caret on second arg`() {
+        checkByText("""
+f : String -> Char -> Int
+f x y = 42
+main = f "x" {-caret-}
+""", "f : String → Char → Int")
+    }
+
+    fun `test function with two args nested in another function call`() {
+        checkByText("""
+f : Int -> Int
+f x = 42
+g : Char -> Bool -> Int
+g x y = 99
+main = f (g 'x' {-caret-})
+""", "g : Char → Bool → Int")
+    }
+
+
+    // UTILS
+
+
+    private fun checkByText(@Language("Elm") code: String, hint: String) {
+        myFixture.configureByText("main.elm", replaceCaretMarker(code))
+        val handler = ElmParameterInfoHandler()
+        val createContext = MockCreateParameterInfoContext(myFixture.editor, myFixture.file)
+
+        // Check hint
+        val elt = handler.findElementForParameterInfo(createContext)
+        if (hint.isNotEmpty()) {
+            elt ?: throw AssertionFailedError("Hint not found")
+            handler.showParameterInfo(elt, createContext)
+            val items = createContext.itemsToShow ?: throw AssertionFailedError("Parameters are not shown")
+            if (items.isEmpty()) throw AssertionFailedError("Parameters are empty")
+            val context = MockParameterInfoUIContext(elt)
+            handler.updateUI(items[0] as ElmParametersDescription, context)
+            TestCase.assertEquals(hint, handler.hintText)
+
+            // Check parameter index
+            val updateContext = MockUpdateParameterInfoContext(myFixture.editor, myFixture.file)
+            val element = handler.findElementForUpdatingParameterInfo(updateContext)
+                    ?: throw AssertionFailedError("Parameter not found")
+            TestCase.assertNotNull(element)
+        } else if (elt != null) {
+            throw AssertionFailedError("Unexpected hint found")
+        }
+    }
+}
