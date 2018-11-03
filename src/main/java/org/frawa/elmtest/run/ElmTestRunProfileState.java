b0b106d85850db2fcce6cfe0ceab43ddcb250edf
Frank Wagner <agilecoderfrank@gmail.com>
2018-11-03 17:47:19 +0100

    colored elm test output

--- src/main/java/org/frawa/elmtest/run/ElmTestRunProfileState.java
+++ src/main/java/org/frawa/elmtest/run/ElmTestRunProfileState.java
@@ -1,12 +1,15 @@
 package org.frawa.elmtest.run;
 
 import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.CommandLineState;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.process.ColoredProcessHandler;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.ui.ConsoleView;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ElmTestRunProfileState extends CommandLineState {
 
@@ -17,10 +20,14 @@ public class ElmTestRunProfileState extends CommandLineState {
     @NotNull
     @Override
     protected ProcessHandler startProcess() throws ExecutionException {
-        GeneralCommandLine commandLine = new GeneralCommandLine("/usr/local/bin/elm", "test")
+//        GeneralCommandLine commandLine = new GeneralCommandLine("/usr/local/bin/elm", "test")
+//        GeneralCommandLine commandLine = new GeneralCommandLine("/bin/sh", "-i", "-c", "elm test")
+        GeneralCommandLine commandLine = new GeneralCommandLine("/usr/bin/script", "-q", "/dev/null", "elm", "test")
                 .withWorkDirectory(this.getEnvironment().getProject().getBasePath())
                 .withRedirectErrorStream(true)
-                .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE);
+                .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+                .withEnvironment("TERM", "xterm-256color")
+                .withEnvironment("COLORTERM", "truecolor");
         return new ColoredProcessHandler(commandLine);
     }
 }
