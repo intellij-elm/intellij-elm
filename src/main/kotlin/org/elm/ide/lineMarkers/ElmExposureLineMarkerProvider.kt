6f69cde96c74da18c453dcc7a1ab0374d21b885c
Keith Lazuka <kelazuka@microsoft.com>
2018-12-27 09:54:31 -0800

    Click the gutter icon to go to where it is exposed

--- src/main/kotlin/org/elm/ide/lineMarkers/ElmExposureLineMarkerProvider.kt
+++ src/main/kotlin/org/elm/ide/lineMarkers/ElmExposureLineMarkerProvider.kt
@@ -1,9 +1,8 @@
 package org.elm.ide.lineMarkers
 
-import com.intellij.codeHighlighting.Pass
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
-import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.psi.PsiElement
 import org.elm.ide.icons.ElmIcons
 import org.elm.lang.core.psi.ElmFile
@@ -15,7 +14,6 @@ import org.elm.lang.core.psi.elements.ElmFunctionDeclarationLeft
 import org.elm.lang.core.psi.elements.ElmTypeAliasDeclaration
 import org.elm.lang.core.psi.elements.ElmTypeDeclaration
 import org.elm.lang.core.psi.elements.findMatchingItemFor
-import java.awt.event.MouseEvent
 
 /**
  * Put an icon in the gutter for top-level declarations (types, functions, values)
@@ -26,17 +24,17 @@ class ElmExposureLineMarkerProvider : LineMarkerProvider {
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<PsiElement>? {
         if (element.elementType !in listOf(LOWER_CASE_IDENTIFIER, UPPER_CASE_IDENTIFIER)) return null
 
-        val parent = element.parent
-        if (parent !is ElmNameIdentifierOwner || parent.nameIdentifier != element) return null
+        val parentDecl = element.parent
+        if (parentDecl !is ElmNameIdentifierOwner || parentDecl.nameIdentifier != element) return null
 
-        return when (parent) {
+        return when (parentDecl) {
             is ElmFunctionDeclarationLeft ->
-                if (!parent.isTopLevel) null
-                else makeMarkerIfNecessary(element, parent)
+                if (!parentDecl.isTopLevel) null
+                else makeMarkerIfExposed(element, parentDecl)
 
             is ElmTypeDeclaration,
             is ElmTypeAliasDeclaration ->
-                makeMarkerIfNecessary(element, parent)
+                makeMarkerIfExposed(element, parentDecl)
 
             else ->
                 null
@@ -49,26 +47,23 @@ class ElmExposureLineMarkerProvider : LineMarkerProvider {
 }
 
 
-private fun makeMarkerIfNecessary(element: PsiElement, decl: ElmNameIdentifierOwner): LineMarkerInfo<PsiElement>? {
+private fun makeMarkerIfExposed(element: PsiElement, decl: ElmNameIdentifierOwner): LineMarkerInfo<PsiElement>? {
     val elmFile = element.containingFile as? ElmFile ?: return null
     val moduleDecl = elmFile.getModuleDecl() ?: return null
     val exposingList = moduleDecl.exposingList ?: return null
     if (moduleDecl.exposesAll) {
-        return makeMarker(element) { _, _ -> Unit }
+        return makeMarker(element, exposingList.doubleDot)
     } else {
         val item = exposingList.findMatchingItemFor(decl) ?: return null
-        return makeMarker(element) { _, _ -> Unit }
+        return makeMarker(element, item)
     }
 }
 
 
-private fun makeMarker(element: PsiElement, navHandler: (MouseEvent, PsiElement) -> Unit) =
-        LineMarkerInfo(
-                element,
-                element.textRange,
-                ElmIcons.EXPOSED_GUTTER,
-                Pass.LINE_MARKERS,
-                { "Exposed" },
-                navHandler,
-                GutterIconRenderer.Alignment.RIGHT
-        )
+private fun makeMarker(element: PsiElement, target: PsiElement?) =
+        NavigationGutterIconBuilder
+                .create(ElmIcons.EXPOSED_GUTTER)
+                .setTarget(target)
+                .setPopupTitle("Go to where it is exposed")
+                .setTooltipText("Exposed")
+                .createLineMarkerInfo(element)
