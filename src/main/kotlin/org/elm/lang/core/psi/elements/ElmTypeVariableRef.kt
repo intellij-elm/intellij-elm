3bbf86782327d3bab3f665b3b82471d64c1d7b7d
Keith Lazuka <kelazuka@microsoft.com>
2019-02-25 07:36:40 -0800

    Remove usage of IntelliJ API not present in WebStorm

--- src/main/kotlin/org/elm/lang/core/psi/elements/ElmTypeVariableRef.kt
+++ src/main/kotlin/org/elm/lang/core/psi/elements/ElmTypeVariableRef.kt
@@ -2,7 +2,6 @@ package org.elm.lang.core.psi.elements
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.parentOfType
 import org.elm.lang.core.psi.*
 import org.elm.lang.core.resolve.ElmReferenceElement
 import org.elm.lang.core.resolve.reference.ElmReferenceCached
