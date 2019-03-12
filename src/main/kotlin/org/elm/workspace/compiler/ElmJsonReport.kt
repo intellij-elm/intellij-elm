51af46b74983951884003af37c88070c04f9f08f
Keith Lazuka <kelazuka@microsoft.com>
2019-03-11 20:20:45 -0700

    Refactor elmJsonToCompilerMessages

--- src/main/kotlin/org/elm/workspace/compiler/ElmJsonReport.kt
+++ src/main/kotlin/org/elm/workspace/compiler/ElmJsonReport.kt
@@ -5,59 +5,60 @@ import java.util.regex.Pattern
 
 private val urlPattern = Pattern.compile(".*<((http|https)(://.*))>.*", Pattern.CASE_INSENSITIVE or Pattern.DOTALL)
 private const val nonBreakingSpace = "&nbsp;"
-private val tempAnchorReplacement = "##TEMP##"
+private const val tempAnchorReplacement = "##TEMP##"
 
 
 fun elmJsonToCompilerMessages(json: String): List<CompilerMessage> {
     val gson = GsonBuilder().setPrettyPrinting().create()
     val obj = gson.fromJson<JsonObject>(json, JsonObject::class.java)
-    val type = obj.get("type").asString
-    if (type == "error") {
-        val path = obj.get("path").asString ?: "-no path-"
-        val title = obj.get("title").asString
-        return listOf(CompilerMessage(title, path, createHtmlMessage(obj.get("message").asJsonArray, title)))
-    } else {
-        val errors = obj.get("errors").asJsonArray
-        val result: MutableList<CompilerMessage> = mutableListOf()
-        errors?.forEach { error ->
-            run {
-                val path = error.asJsonObject.get("path").asString ?: "-no path-"
-                val name = error.asJsonObject.get("name").asString ?: "-no name-"
-                val problems = error.asJsonObject.get("problems").asJsonArray
-                val list = problemsMessageAndRegion(problems).map { CompilerMessage(name, path, it) }
-                result.addAll(list)
+    val reportType = obj["type"].asString
+    return when (reportType) {
+        "error" -> {
+            val path = obj["path"].asString
+            val title = obj["title"].asString
+            listOf(CompilerMessage(title, path,
+                    createMessageAndRegion(obj["message"].asJsonArray, title, Region(End(0, 0), Start(0, 0)))))
+        }
+        "compile-errors" -> {
+            obj["errors"].asJsonArray.flatMap { error ->
+                val path = error.asJsonObject["path"].asString
+                val name = error.asJsonObject["name"].asString
+                error.asJsonObject["problems"].asJsonArray.map {
+                    CompilerMessage(name, path,
+                            createMessageAndRegion(
+                                    it.asJsonObject["message"].asJsonArray,
+                                    it.asJsonObject["title"].asString,
+                                    Gson().fromJson(it.asJsonObject["region"].asJsonObject.toString(), Region::class.java)
+                            ))
+                }
             }
         }
-        return result
+        else ->
+            error("Unexpected Elm compiler report type '$reportType'")
     }
 }
 
-private fun createHtmlMessage(message: JsonArray, title: String): MessageAndRegion {
-    val lines = message.map { (::itemToString)(it) }
-    val body = lines.joinToString("")
-    return MessageAndRegion("<html><body style=\"font-family: monospace\">$body</body></html>", Region(End(0, 0), Start(0, 0)), title)
-}
 
-private fun problemsMessageAndRegion(problems: com.google.gson.JsonArray): List<MessageAndRegion> {
-    return problems.map {
-        createHtmlMessageAndRegion(it.asJsonObject.get("message").asJsonArray, it.asJsonObject.get("region").asJsonObject, it.asJsonObject.get("title").asString)
-    }
-}
-
-private fun createHtmlMessageAndRegion(message: JsonArray, region: JsonObject, title: String): MessageAndRegion {
-    val reg = Gson().fromJson(region.toString(), Region::class.java)
-    val lines = message.map { (::itemToString)(it) }
-    val body = lines.joinToString("")
-    return MessageAndRegion("<html><body style=\"font-family: monospace; font-weight: bold\">$body</body></html>", reg, title)
+private fun createMessageAndRegion(message: JsonArray, title: String, region: Region): MessageAndRegion {
+    val html = message.joinToString("",
+            prefix = "<html><body style=\"font-family: monospace; font-weight: bold\">",
+            postfix = "</body></html>"
+    ) { itemToString(it) }
+    return MessageAndRegion(html, region, title)
 }
 
 private fun itemToString(jsonElement: JsonElement): String {
+    // Each item is either a String or an object containing the string as well as some formatting options
     if (jsonElement.isJsonPrimitive) {
         val urlMatcher = urlPattern.matcher(jsonElement.asString)
         if (urlMatcher.find()) {
-            val httpurl = urlMatcher.group(1)
-            val anchor = "<a href=\"$httpurl\">$httpurl</a>"
-            return asTextSpan(jsonElement.asString.replace("<$httpurl>", tempAnchorReplacement).replace(" ", nonBreakingSpace).replace("\n", "<br>").replace(tempAnchorReplacement, anchor))
+            val httpUrl = urlMatcher.group(1)
+            val anchor = "<a href=\"$httpUrl\">$httpUrl</a>"
+            return asTextSpan(jsonElement.asString
+                    .replace("<$httpUrl>", tempAnchorReplacement)
+                    .replace(" ", nonBreakingSpace)
+                    .replace("\n", "<br>")
+                    .replace(tempAnchorReplacement, anchor))
         }
         return asTextSpan(jsonElement.asString.replace(" ", nonBreakingSpace).replace("\n", "<br>"))
     } else
@@ -79,13 +80,12 @@ private fun markupToString(jsonObject: JsonObject): String {
             if (!it.isJsonNull) styleBuilder.append("color: ${mapColor(it.asString)};") else styleBuilder.append("color: #A1A8B3;")
         }
     }
-    return "<span style=\"$styleBuilder\">${jsonObject.get("string").asString.replace(" ", nonBreakingSpace)}</span>"
+    return "<span style=\"$styleBuilder\">${jsonObject["string"].asString.replace(" ", nonBreakingSpace)}</span>"
 }
 
-private fun mapColor(color: String): String {
-    when (color) {
-        "yellow" -> return "#FACF5A"
-        "red" -> return "#FF5959"
-    }
-    return color
-}
+private fun mapColor(color: String): String =
+        when (color) {
+            "yellow" -> "#FACF5A"
+            "red" -> "#FF5959"
+            else -> color
+        }
