e49863915a753b74dff36226bf057ba258333337
Keith Lazuka <kelazuka@microsoft.com>
2019-03-11 21:50:53 -0700

    Use Gson to parse directly into rich data types

--- src/main/kotlin/org/elm/workspace/compiler/ElmJsonReport.kt
+++ src/main/kotlin/org/elm/workspace/compiler/ElmJsonReport.kt
@@ -1,87 +1,63 @@
 package org.elm.workspace.compiler
 
-import com.google.gson.*
 import java.util.regex.Pattern
 
 private val urlPattern = Pattern.compile(".*<((http|https)(://.*))>.*", Pattern.CASE_INSENSITIVE or Pattern.DOTALL)
 private const val nonBreakingSpace = "&nbsp;"
 private const val tempAnchorReplacement = "##TEMP##"
+private val dummyRegion = Region(Start(0, 0), End(0, 0))
 
 
 fun elmJsonToCompilerMessages(json: String): List<CompilerMessage> {
-    val gson = GsonBuilder().setPrettyPrinting().create()
-    val obj = gson.fromJson<JsonObject>(json, JsonObject::class.java)
-    val reportType = obj["type"].asString
-    return when (reportType) {
-        "error" -> {
-            val path = obj["path"].asString
-            val title = obj["title"].asString
-            listOf(CompilerMessage(title, path,
-                    createMessageAndRegion(obj["message"].asJsonArray, title, Region(End(0, 0), Start(0, 0)))))
+    val report = newGson().fromJson(json, Report::class.java) ?: error("failed to parse JSON report from elm")
+    return when (report) {
+        is Report.General -> {
+            listOf(CompilerMessage(report.title, report.path ?: "",
+                    MessageAndRegion(chunksToHtml(report.message), dummyRegion, report.title)))
         }
-        "compile-errors" -> {
-            obj["errors"].asJsonArray.flatMap { error ->
-                val path = error.asJsonObject["path"].asString
-                val name = error.asJsonObject["name"].asString
-                error.asJsonObject["problems"].asJsonArray.map {
-                    CompilerMessage(name, path,
-                            createMessageAndRegion(
-                                    it.asJsonObject["message"].asJsonArray,
-                                    it.asJsonObject["title"].asString,
-                                    Gson().fromJson(it.asJsonObject["region"].asJsonObject.toString(), Region::class.java)
-                            ))
+        is Report.Specific -> {
+            report.errors.flatMap { error ->
+                error.problems.map { problem ->
+                    CompilerMessage(error.name, error.path,
+                            MessageAndRegion(chunksToHtml(problem.message), problem.region, problem.title))
                 }
             }
         }
-        else ->
-            error("Unexpected Elm compiler report type '$reportType'")
     }
 }
 
+private fun chunksToHtml(chunks: List<Chunk>): String =
+        chunks.joinToString("",
+                prefix = "<html><body style=\"font-family: monospace; font-weight: bold\">",
+                postfix = "</body></html>"
+        ) { chunkToHtml(it) }
 
-private fun createMessageAndRegion(message: JsonArray, title: String, region: Region): MessageAndRegion {
-    val html = message.joinToString("",
-            prefix = "<html><body style=\"font-family: monospace; font-weight: bold\">",
-            postfix = "</body></html>"
-    ) { itemToString(it) }
-    return MessageAndRegion(html, region, title)
-}
-
-private fun itemToString(jsonElement: JsonElement): String {
-    // Each item is either a String or an object containing the string as well as some formatting options
-    if (jsonElement.isJsonPrimitive) {
-        val urlMatcher = urlPattern.matcher(jsonElement.asString)
-        if (urlMatcher.find()) {
-            val httpUrl = urlMatcher.group(1)
-            val anchor = "<a href=\"$httpUrl\">$httpUrl</a>"
-            return asTextSpan(jsonElement.asString
-                    .replace("<$httpUrl>", tempAnchorReplacement)
-                    .replace(" ", nonBreakingSpace)
-                    .replace("\n", "<br>")
-                    .replace(tempAnchorReplacement, anchor))
+private fun chunkToHtml(chunk: Chunk): String =
+        when (chunk) {
+            is Chunk.Unstyled -> {
+                val urlMatcher = urlPattern.matcher(chunk.string)
+                if (urlMatcher.find()) {
+                    val httpUrl = urlMatcher.group(1)
+                    val anchor = "<a href=\"$httpUrl\">$httpUrl</a>"
+                    asTextSpan(chunk.string
+                            .replace("<$httpUrl>", tempAnchorReplacement)
+                            .replace(" ", nonBreakingSpace)
+                            .replace("\n", "<br>")
+                            .replace(tempAnchorReplacement, anchor))
+                } else {
+                    asTextSpan(chunk.string.replace(" ", nonBreakingSpace).replace("\n", "<br>"))
+                }
+            }
+            is Chunk.Styled -> {
+                val styleBuilder = StringBuilder()
+                if (chunk.bold) styleBuilder.append("font-weight: bold;")
+                if (chunk.underline) styleBuilder.append("text-decoration: underline;")
+                styleBuilder.append("color: ${mapColor(chunk.color)};")
+                "<span style=\"$styleBuilder\">${chunk.string.replace(" ", nonBreakingSpace)}</span>"
+            }
         }
-        return asTextSpan(jsonElement.asString.replace(" ", nonBreakingSpace).replace("\n", "<br>"))
-    } else
-        return markupToString(jsonElement.asJsonObject)
-}
 
 private fun asTextSpan(text: String) = "<span style='color: #4F9DA6'>$text</span>"
-
-private fun markupToString(jsonObject: JsonObject): String {
-    val styleBuilder = StringBuilder()
-    with(jsonObject) {
-        get("bold").let {
-            if (!it.isJsonNull && it.asBoolean) styleBuilder.append("font-weight: bold;")
-        }
-        get("underline").let {
-            if (!it.isJsonNull && it.asBoolean) styleBuilder.append("text-decoration: underline;")
-        }
-        get("color").let {
-            if (!it.isJsonNull) styleBuilder.append("color: ${mapColor(it.asString)};") else styleBuilder.append("color: #A1A8B3;")
-        }
-    }
-    return "<span style=\"$styleBuilder\">${jsonObject["string"].asString.replace(" ", nonBreakingSpace)}</span>"
-}
 
 private fun mapColor(color: String): String =
         when (color) {
--- src/main/kotlin/org/elm/workspace/compiler/ElmMakeTypes.kt
+++ src/main/kotlin/org/elm/workspace/compiler/ElmMakeTypes.kt
@@ -1,18 +1,78 @@
 package org.elm.workspace.compiler
 
+import com.google.gson.*
+import java.lang.reflect.Type
+
 // types for Elm's json report
+// The naming is based on https://package.elm-lang.org/packages/elm/project-metadata-utils/latest/Elm-Error
 
-data class Region(val end: End, val start: Start)
+fun newGson(): Gson =
+        GsonBuilder()
+                .registerTypeAdapter(Report::class.java, ReportDeserializer())
+                .registerTypeAdapter(Chunk::class.java, ChunkDeserializer())
+                .create()
 
+sealed class Report {
+    data class General(
+            val path: String?,
+            val title: String,
+            val message: List<Chunk>
+    ) : Report()
+
+    data class Specific(
+            val errors: List<BadModuleError>
+    ) : Report()
+}
+
+class ReportDeserializer : JsonDeserializer<Report> {
+    override fun deserialize(element: JsonElement, typeOf: Type, context: JsonDeserializationContext): Report {
+        val gson = newGson()
+        if (!element.isJsonObject) throw JsonParseException("Expected a report object")
+        val obj = element.asJsonObject
+        val reportType = obj["type"].asString
+        return when (reportType) {
+            "error" -> gson.fromJson(obj, Report.General::class.java)
+            "compile-errors" -> gson.fromJson(obj, Report.Specific::class.java)
+            else -> error("Unexpected Elm compiler report type '$reportType'")
+        }
+    }
+}
+
+data class BadModuleError(
+        val path: String,
+        val name: String,
+        val problems: List<Problem>
+)
+
+data class Problem(
+        val title: String,
+        val region: Region,
+        val message: List<Chunk>
+)
+
+data class Region(val start: Start, val end: End)
 data class Start(val column: Int, val line: Int)
-
 data class End(val column: Int, val line: Int)
+
+sealed class Chunk {
+    data class Unstyled(val string: String) : Chunk()
+    data class Styled(val bold: Boolean, val underline: Boolean, val color: String, val string: String) : Chunk()
+}
+
+class ChunkDeserializer : JsonDeserializer<Chunk> {
+    override fun deserialize(element: JsonElement, typeOf: Type, context: JsonDeserializationContext): Chunk {
+        val gson = Gson()
+        return when {
+            element.isJsonObject -> gson.fromJson(element, Chunk.Styled::class.java)
+            element.isJsonPrimitive -> Chunk.Unstyled(element.asString)
+            else -> throw JsonParseException("Expected a simple string or a rich-text chunk")
+        }
+    }
+}
 
 
 // plugin's types
 
-data class MessageAndRegion(val message: String, val region: Region, val title: String)
-
 data class CompilerMessage(val name: String, val path: String, val messageWithRegion: MessageAndRegion)
 
-
+data class MessageAndRegion(val message: String, val region: Region, val title: String)
