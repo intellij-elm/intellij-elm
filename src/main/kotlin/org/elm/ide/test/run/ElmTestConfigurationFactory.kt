0dafcddacdeb51b3781b782131ffed9e33ea8517
Keith Lazuka <kelazuka@microsoft.com>
2019-12-23 12:28:26 -0500

    Cleanup ElmTestConfigurationFactory

--- src/main/kotlin/org/elm/ide/test/run/ElmTestConfigurationFactory.kt
+++ src/main/kotlin/org/elm/ide/test/run/ElmTestConfigurationFactory.kt
@@ -2,31 +2,19 @@ package org.elm.ide.test.run
 
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
-import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.project.Project
 import org.elm.ide.icons.ElmIcons
-import javax.swing.Icon
 
 class ElmTestConfigurationFactory internal constructor(type: ConfigurationType) : ConfigurationFactory(type) {
 
-    override fun createTemplateConfiguration(project: Project): RunConfiguration {
-        return ElmTestRunConfiguration(project, this, "Elm Test")
-    }
+    override fun createTemplateConfiguration(project: Project) =
+            ElmTestRunConfiguration(project, this, "Elm Test")
 
-    override fun getName(): String {
-        return FACTORY_NAME
-    }
+    override fun getName() = "Elm Test configuration factory"
 
-    override fun getIcon(): Icon? {
-        return RUN_ICON
-    }
+    override fun getIcon() = RUN_ICON
 
     companion object {
-
-        private val FACTORY_NAME = "Elm Test configuration factory"
-
-//        internal
         val RUN_ICON = ElmIcons.COLORFUL
     }
-
 }
