362a5fec7923237461bb1b747b00af387e63a1a2
Frank Wagner <agilecoderfrank@gmail.com>
2019-12-23 16:20:37 +0100

    follow changes since PR was created

--- src/main/kotlin/org/elm/ide/test/run/ElmTestRunConfigurationProducer.kt
+++ src/main/kotlin/org/elm/ide/test/run/ElmTestRunConfigurationProducer.kt
@@ -1,18 +1,20 @@
 package org.elm.ide.test.run
 
 import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.LazyRunConfigurationProducer
 import com.intellij.execution.actions.RunConfigurationProducer
+import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
 import org.elm.ide.test.core.ElmProjectTestsHelper
 import org.elm.workspace.ElmWorkspaceService
 
-class ElmTestRunConfigurationProducer : RunConfigurationProducer<ElmTestRunConfiguration> {
+class ElmTestRunConfigurationProducer : LazyRunConfigurationProducer<ElmTestRunConfiguration>() {
 
-    constructor(configurationFactory: ElmTestConfigurationFactory) : super(configurationFactory)
-
-    constructor(configurationType: ElmTestRunConfigurationType) : super(configurationType)
+    override fun getConfigurationFactory(): ConfigurationFactory {
+        return ElmTestConfigurationFactory(ElmTestRunConfigurationType())
+    }
 
     override fun setupConfigurationFromContext(configuration: ElmTestRunConfiguration, context: ConfigurationContext, sourceElement: Ref<PsiElement>): Boolean {
         return getCandidateElmFolder(context)
@@ -37,7 +39,6 @@ class ElmTestRunConfigurationProducer : RunConfigurationProducer<ElmTestRunConfi
 
         val elmWorkspace = ServiceManager.getService(
                 context.project, ElmWorkspaceService::class.java)
-
 
         return context.location?.virtualFile
                 ?.let {
