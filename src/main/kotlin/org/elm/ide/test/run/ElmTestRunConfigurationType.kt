02dc77115ed2dde36e687961e7f5a11c11d9ce62
Keith Lazuka <kelazuka@microsoft.com>
2019-12-23 16:00:09 -0500

    Cleanup ElmTestRunConfigurationType.kt

--- src/main/kotlin/org/elm/ide/test/run/ElmTestRunConfigurationType.kt
+++ src/main/kotlin/org/elm/ide/test/run/ElmTestRunConfigurationType.kt
@@ -1,31 +1,18 @@
 package org.elm.ide.test.run
 
-import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
 import org.elm.ide.test.run.ElmTestConfigurationFactory.Companion.RUN_ICON
-import javax.swing.Icon
 
 class ElmTestRunConfigurationType : ConfigurationType {
-    override fun getDisplayName(): String {
-        return "Elm Test"
-    }
 
-    override fun getConfigurationTypeDescription(): String {
-        return "Elm Test Runner"
-    }
+    override fun getDisplayName() = "Elm Test"
 
-    override fun getIcon(): Icon {
-        return RUN_ICON
-    }
+    override fun getConfigurationTypeDescription() = "Elm Test Runner"
 
-    override fun getId(): String {
-        return "ELM_TEST_RUN_CONFIGURATION"
-    }
+    override fun getIcon() = RUN_ICON
 
-    override fun getConfigurationFactories(): Array<ConfigurationFactory> {
-        return arrayOf(ElmTestConfigurationFactory(this))
-    }
+    override fun getId() = "ELM_TEST_RUN_CONFIGURATION"
 
+    override fun getConfigurationFactories() = arrayOf(ElmTestConfigurationFactory(this))
 
 }
-
