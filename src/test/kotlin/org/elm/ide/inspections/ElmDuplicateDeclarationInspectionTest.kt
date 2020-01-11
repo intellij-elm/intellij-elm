b72e44a2ca1cdf4de7f998f253a13e4838c8f5f4
Keith Lazuka <klazuka@gmail.com>
2020-01-11 15:43:25 -0500

    Forgot to update the tests

--- src/test/kotlin/org/elm/ide/inspections/ElmDuplicateDeclarationInspectionTest.kt
+++ src/test/kotlin/org/elm/ide/inspections/ElmDuplicateDeclarationInspectionTest.kt
@@ -8,18 +8,18 @@ foo = Foo
 """)
 
     fun `test function dupes`() = checkByText("""
-<error descr="Multiple declarations with name foo">foo</error> = ()
-<error descr="Multiple declarations with name bar">bar</error> = ()
+<error descr="Multiple declarations with name 'foo'">foo</error> = ()
+<error descr="Multiple declarations with name 'bar'">bar</error> = ()
 baz = ()
-port <error descr="Multiple declarations with name foo">foo</error> : () -> ()
-<error descr="Multiple declarations with name bar">bar</error> = ()
-<error descr="Multiple declarations with name foo">foo</error> = ()
+port <error descr="Multiple declarations with name 'foo'">foo</error> : () -> ()
+<error descr="Multiple declarations with name 'bar'">bar</error> = ()
+<error descr="Multiple declarations with name 'foo'">foo</error> = ()
 """)
 
     fun `test type dupes`() = checkByText("""
-type <error descr="Multiple declarations with name Foo">Foo</error> = Foo
+type <error descr="Multiple declarations with name 'Foo'">Foo</error> = Foo
 type Bar = Foo
-type alias <error descr="Multiple declarations with name Foo">Foo</error> = ()
-type alias <error descr="Multiple declarations with name Foo">Foo</error> = ()
+type alias <error descr="Multiple declarations with name 'Foo'">Foo</error> = ()
+type alias <error descr="Multiple declarations with name 'Foo'">Foo</error> = ()
 """)
 }
