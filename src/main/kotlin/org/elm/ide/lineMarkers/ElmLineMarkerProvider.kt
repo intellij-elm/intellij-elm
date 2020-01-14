de1d83a00bcc657f470605222f9b0bdf529f6699
AJ Alt <ajalt@users.noreply.github.com>
2020-01-14 11:37:48 -0800

    Add options to disable individual line marker types (#591)
    This consolidates all line marker providers into a single LineMarkerProviderDescriptor implementation that calls out to the individual providers and handles registering and checking options, as well as the boilerplate for `collectSlowLineMarkers`. Individual providers can just implement `getLineMarkerInfo`. Providers are created for each call to `collectSlowLineMarkers`, so they can have internal state if they need to.
    
    Fixes #590

---
+++ src/main/kotlin/org/elm/ide/lineMarkers/ElmLineMarkerProvider.kt
@@ -0,0 +1,44 @@
+package org.elm.ide.lineMarkers
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.DumbService
+import com.intellij.psi.PsiElement
+import org.elm.lang.core.psi.ElmFile
+import org.elm.lang.core.psi.parentOfType
+
+class ElmLineMarkerProvider : LineMarkerProviderDescriptor() {
+    companion object {
+        private val optionProviders = mapOf(
+                ElmExposureLineMarkerProvider.OPTION to { ElmExposureLineMarkerProvider() },
+                ElmRecursiveCallLineMarkerProvider.OPTION to { ElmRecursiveCallLineMarkerProvider() }
+        )
+        private val OPTIONS = optionProviders.keys.toTypedArray()
+    }
+
+    override fun getName(): String? = "Elm line markers"
+    override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? = null
+
+    // This provides the options to show in Settings > Editor > General > Gutter Icons
+    override fun getOptions(): Array<Option> = OPTIONS
+
+    override fun collectSlowLineMarkers(elements: List<PsiElement>, result: MutableCollection<LineMarkerInfo<PsiElement>>) {
+        val first = elements.firstOrNull() ?: return
+        if (DumbService.getInstance(first.project).isDumb || first.parentOfType<ElmFile>()?.elmProject == null) return
+
+        // Create the providers each time this function is called, since they may have internal state
+        val providers = optionProviders.filter { it.key.isEnabled }.values.map { it() }
+
+        if (providers.isEmpty()) return
+
+        for (element in elements) {
+            ProgressManager.checkCanceled()
+
+            // See the docs of LineMarkerProvider for why we can only add markers to leaf nodes
+            if (element.firstChild != null) continue
+
+            providers.mapNotNullTo(result) { it.getLineMarkerInfo(element) }
+        }
+    }
+}
--- src/main/kotlin/org/elm/ide/lineMarkers/ElmRecursiveCallLineMarkerProvider.kt
+++ src/main/kotlin/org/elm/ide/lineMarkers/ElmRecursiveCallLineMarkerProvider.kt
@@ -1,60 +1,57 @@
 package org.elm.ide.lineMarkers
 
+import com.intellij.codeInsight.daemon.GutterIconDescriptor
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
-import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.util.FunctionUtil
 import org.elm.ide.icons.ElmIcons
-import org.elm.lang.core.psi.*
 import org.elm.lang.core.psi.ElmTypes.LOWER_CASE_IDENTIFIER
-import org.elm.lang.core.psi.ElmTypes.UPPER_CASE_IDENTIFIER
-import org.elm.lang.core.psi.elements.*
-import org.elm.lang.core.resolve.ElmReferenceElement
+import org.elm.lang.core.psi.ancestorsStrict
+import org.elm.lang.core.psi.elementType
+import org.elm.lang.core.psi.elements.ElmFunctionCallExpr
+import org.elm.lang.core.psi.elements.ElmValueDeclaration
+import org.elm.lang.core.psi.elements.ElmValueExpr
+import org.elm.lang.core.psi.elements.ElmValueQID
 
 /**
  * Put an icon in the gutter for top-level declarations (types, functions, values)
  * that are exposed by the containing module.
  */
 class ElmRecursiveCallLineMarkerProvider : LineMarkerProvider {
+    companion object {
+        val OPTION = GutterIconDescriptor.Option("elm.recursive", "Recursive call", ElmIcons.RECURSIVE_CALL)
+    }
 
-    override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? = null
+    private val lines = mutableSetOf<Int>() // Only one marker per line
 
-    override fun collectSlowLineMarkers(
-            elements: List<PsiElement>,
-            result: MutableCollection<LineMarkerInfo<PsiElement>>
-    ) {
-        // we use the slow pass since we need to resolve references
+    override fun getLineMarkerInfo(el: PsiElement): LineMarkerInfo<*>? {
+        // We only call this from ElmLineMarkerProvider.collectSlowLineMarkers, so it's ok to resolve references
+        if (el.elementType != LOWER_CASE_IDENTIFIER) return null
+        val qid = el.parent as? ElmValueQID ?: return null
+        if (qid.qualifiers.isNotEmpty()) return null
+        val valueExpr = qid.parent as? ElmValueExpr ?: return null
+        val functionCall = valueExpr.parent as? ElmFunctionCallExpr ?: return null
+        if (functionCall.target != valueExpr) return null
 
-        val lines = mutableSetOf<Int>() // Only one marker per line
+        val ref = valueExpr.reference.resolve()
+        val nearestFunc = functionCall.ancestorsStrict.filterIsInstance<ElmValueDeclaration>()
+                .firstOrNull()?.functionDeclarationLeft
+        if (nearestFunc != ref) return null // not recursive
 
-        for (el in elements) {
-            if (el.elementType != LOWER_CASE_IDENTIFIER) continue
-            val qid = el.parent as? ElmValueQID ?: continue
-            if (qid.qualifiers.isNotEmpty()) continue
-            val valueExpr = qid.parent as? ElmValueExpr ?: continue
-            val functionCall = valueExpr.parent as? ElmFunctionCallExpr ?: continue
-            if (functionCall.target != valueExpr) continue
-
-            val ref = valueExpr.reference.resolve()
-            val nearestFunc = functionCall.ancestorsStrict.filterIsInstance<ElmValueDeclaration>()
-                    .firstOrNull()?.functionDeclarationLeft
-            if (nearestFunc != ref) continue // not recursive
-
-            val doc = PsiDocumentManager.getInstance(el.project).getDocument(el.containingFile) ?: continue
-            val lineNumber = doc.getLineNumber(el.textOffset)
-            if (lines.add(lineNumber)) {
-                result.add(LineMarkerInfo(
-                        el,
-                        el.textRange,
-                        ElmIcons.RECURSIVE_CALL,
-                        FunctionUtil.constant("Recursive call"),
-                        null,
-                        GutterIconRenderer.Alignment.RIGHT)
-                )
-            }
+        val doc = PsiDocumentManager.getInstance(el.project).getDocument(el.containingFile) ?: return null
+        val lineNumber = doc.getLineNumber(el.textOffset)
+        if (lines.add(lineNumber)) {
+            return LineMarkerInfo(
+                    el,
+                    el.textRange,
+                    ElmIcons.RECURSIVE_CALL,
+                    FunctionUtil.constant("Recursive call"),
+                    null,
+                    GutterIconRenderer.Alignment.RIGHT)
         }
+        return null
     }
 }
