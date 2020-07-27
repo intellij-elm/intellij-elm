cbeee6b46bbf8e4da532b366c7ebcc8873a75188
Dillon Kearns <dillon@dillonkearns.com>
2020-07-27 12:31:27 -0700

    Generalize extraction of AST data.

--- src/main/kotlin/org/elm/ide/intentions/MapToFoldIntention.kt
+++ src/main/kotlin/org/elm/ide/intentions/MapToFoldIntention.kt
@@ -3,10 +3,8 @@ package org.elm.ide.intentions
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import org.elm.lang.core.psi.ElmPsiFactory
 import org.elm.lang.core.psi.ancestors
-import org.elm.lang.core.psi.elements.ElmExposingList
 import org.elm.lang.core.psi.elements.ElmFunctionCallExpr
 
 class MapToFoldIntention : ElmAtCaretIntentionActionBase<MapToFoldIntention.Context>() {
@@ -34,8 +32,14 @@ class MapToFoldIntention : ElmAtCaretIntentionActionBase<MapToFoldIntention.Cont
     override fun invoke(project: Project, editor: Editor, context: Context) {
 
         val elmPsiFactory = ElmPsiFactory(project)
+        val first = context.mapInvocation.arguments.toList().first()
+        val second = context.mapInvocation.arguments.toList().get(1)
 
-        context.mapInvocation.replace(elmPsiFactory.createFunctionCallExpr("List.foldr (\\item result -> f item :: result) [] items"))
+        val functionName = "List.foldr"
+        val innerFunctionName = first.text
+        val itemsExpression = second.text
+        val functionCallText = "$functionName (\\item result -> $innerFunctionName item :: result) [] $itemsExpression"
+        context.mapInvocation.replace(elmPsiFactory.createFunctionCallExpr(functionCallText))
     }
 
 }
