275c028dc7e143815130a7843bc431643f7ae005
Dillon Kearns <dillon@dillonkearns.com>
2020-08-06 08:42:06 -0700

    Remove obsolete hint info since we're only doing type hints for now.

--- src/main/kotlin/org/elm/ide/hints/parameter/ElmInlayParameterHintsProvider.kt
+++ src/main/kotlin/org/elm/ide/hints/parameter/ElmInlayParameterHintsProvider.kt
@@ -10,9 +10,6 @@ import com.intellij.codeInsight.hints.InlayInfo
 import com.intellij.codeInsight.hints.InlayParameterHintsProvider
 import com.intellij.codeInsight.hints.Option
 import com.intellij.psi.PsiElement
-import org.elm.lang.core.ElmLanguage
-import org.elm.lang.core.psi.elements.ElmFunctionCallExpr
-import org.elm.lang.core.psi.elements.ElmFunctionDeclarationLeft
 
 
 @Suppress("UnstableApiUsage")
@@ -22,17 +19,6 @@ class ElmInlayParameterHintsProvider : InlayParameterHintsProvider {
 
     override fun getDefaultBlackList(): Set<String> = emptySet()
 
-    override fun getHintInfo(element: PsiElement): HintInfo? {
-        return when (element) {
-            is ElmFunctionCallExpr -> {
-                resolve(element)
-            }
-            else -> {
-                null
-            }
-        }
-    }
-
     @ExperimentalStdlibApi
     override fun getParameterHints(element: PsiElement): List<InlayInfo> {
         if (ElmInlayParameterHints.enabled) {
@@ -41,18 +27,12 @@ class ElmInlayParameterHintsProvider : InlayParameterHintsProvider {
         return emptyList()
     }
 
-    override fun getInlayPresentation(inlayText: String): String = inlayText
-
-    companion object {
-        private fun resolve(call: ElmFunctionCallExpr): HintInfo.MethodInfo? {
-            val fn = (call.target).reference?.resolve() as? ElmFunctionDeclarationLeft ?: return null
-            val parameters = fn.namedParameters.map { it.text }
-            return createMethodInfo(fn, parameters)
-        }
-
-        private fun createMethodInfo(function: ElmFunctionDeclarationLeft, parameters: List<String>): HintInfo.MethodInfo? {
-            val path = function.name
-            return HintInfo.MethodInfo(path, parameters, ElmLanguage)
-        }
+    override fun getHintInfo(element: PsiElement?): HintInfo? {
+        // Hint info is used to add function names to the exclude list
+        // since we're just doing type hints right now, it doesn't apply.
+        // It will apply if we add parameter name hints.
+        return null
     }
+
+    override fun getInlayPresentation(inlayText: String): String = inlayText
 }
