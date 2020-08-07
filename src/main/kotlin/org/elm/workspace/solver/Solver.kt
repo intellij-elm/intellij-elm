f61a11c3242291777b95e3d650e9ae2155a052ed
Keith Lazuka <klazuka@gmail.com>
2020-08-07 11:53:05 -0400

    Passes some simple tests

--- src/main/kotlin/org/elm/workspace/solver/Solver.kt
+++ src/main/kotlin/org/elm/workspace/solver/Solver.kt
@@ -26,14 +26,6 @@ data class Repository(private val packagesByName: Map<PkgName, List<Pkg>>) {
     }
 }
 
-sealed class SolverResult {
-    object DeadEnd : SolverResult()
-    data class Proceed(
-            val pending: Map<PkgName, Constraint>,
-            val solutions: Map<PkgName, Version>
-    ) : SolverResult()
-}
-
 fun solve(deps: Map<PkgName, Constraint>, repo: Repository): Map<PkgName, Version>? {
     val solutions = mapOf<PkgName, Version>()
     return when (val res = Solver(repo).solve(deps, solutions)) {
@@ -42,26 +34,56 @@ fun solve(deps: Map<PkgName, Constraint>, repo: Repository): Map<PkgName, Versio
     }
 }
 
-class Solver(private val repo: Repository) {
+private sealed class SolverResult {
+    object DeadEnd : SolverResult()
+    data class Proceed(
+            val pending: Map<PkgName, Constraint>,
+            val solutions: Map<PkgName, Version>
+    ) : SolverResult()
+}
+
+private class Solver(private val repo: Repository) {
     fun solve(deps: Map<PkgName, Constraint>, solutions: Map<PkgName, Version>): SolverResult {
-        // Pick a dep/constraint (Elm compiler picks by names in lexicographically ascending order)
-        val dep = deps.minBy { it.key }
+        // Pick a dep to solve
+        val (dep, restDeps) = deps.pick()
         if (dep == null) {
-            // no more dependencies left to explore
+            // Base case: no more dependencies left to explore
             return Proceed(deps, solutions)
         }
 
-        val (pkgName, constraint) = dep
-
         // Figure out which versions of the dep actually exist
-        val candidates = repo[pkgName].filter { it.version.satisfies(constraint) }
-        println("$pkgName has candidates ${candidates.map { it.version }}")
+        var candidates = repo[dep.name]
+                .filter { it.version.satisfies(dep.constraint) }
+                .sortedByDescending { it.version }
+        println("${dep.name} has candidates ${candidates.map { it.version }}")
+
+        // Further restrict the candidates if a pending solution has already bound the version of this dep.
+        val solvedVersion = solutions[dep.name]
+        if (solvedVersion != null) {
+            if (candidates.any { it.version == solvedVersion }) {
+                // TODO [kl] shouldn't this never happen? or if it does, we should be able to immediately move on.
+                println("Already solved ${dep.name} as $solvedVersion")
+            }
+            candidates = candidates.filter { it.version == solvedVersion }
+        }
 
         if (candidates.isEmpty()) {
             return DeadEnd
         }
 
-        for (candidate in candidates) {
+        // Speculatively try each candidate version to see if it is a partial solution
+        loop@ for (candidate in candidates) {
+            // TODO [kl] check that constraints for the same key are compatible when combining
+            val tentativeDeps = restDeps + candidate.dependencies
+
+            // TODO [kl] check that solutions for the same key are equal when combining
+            val tentativeSolutions = solutions + (dep.name to candidate.version)
+
+            // TODO [kl] are we going to overflow the stack when recursing?
+            when (val res = solve(tentativeDeps, tentativeSolutions)) {
+                DeadEnd -> continue@loop
+                is Proceed -> return solve(res.pending, res.solutions)
+            }
         }
 
         return DeadEnd
@@ -69,4 +91,10 @@ class Solver(private val repo: Repository) {
 }
 
 
+private data class Dep(val name: PkgName, val constraint: Constraint)
 
+private fun Map<PkgName, Constraint>.pick(): Pair<Dep?, Map<PkgName, Constraint>> {
+    // Pick a dep/constraint (Elm compiler picks by pkg name in lexicographically ascending order)
+    val dep = minBy { it.key } ?: return (null to this)
+    return Dep(dep.key, dep.value) to minus(dep.key)
+}
--- src/test/kotlin/org/elm/workspace/solver/SolverTest.kt
+++ src/test/kotlin/org/elm/workspace/solver/SolverTest.kt
@@ -16,15 +16,33 @@ private val repo = Repository(
                 dependencies = mapOf(
                         "B" to r("1.0.1 <= v < 1.0.3")
                 )
+        ),
+        Pkg(
+                name = "D",
+                version = v(1, 0, 0),
+                dependencies = mapOf(
+                        "B" to r("1.0.1 <= v < 1.0.2"),
+                        "C" to r("1.0.0 <= v < 2.0.0")
+                )
         )
 )
 
 class SolverTest {
 
     @Test
+    fun `handles empty input`() = doTest(
+            deps = emptyMap(),
+            expect = emptyMap()
+    )
+
+    @Test
     fun `trivial resolve`() = doTest(
             deps = mapOf("C" to r("1.0.0 <= v < 2.0.0")),
-            expect = mapOf("C" to v(1, 0, 0)))
+            expect = mapOf(
+                    "B" to v(1, 0, 2),
+                    "C" to v(1, 0, 0)
+            )
+    )
 
     @Test
     fun `picks highest available version`() = doTest(
@@ -42,6 +60,24 @@ class SolverTest {
                     "B" to v(1, 0, 2),
                     "C" to v(1, 0, 0)
             ))
+
+    @Test
+    fun `resolve mutual constraints - multiple levels`() = doTest(
+            deps = mapOf(
+                    "B" to r("1.0.0 <= v < 1.0.4"),
+                    "C" to r("1.0.0 <= v < 2.0.0"),
+                    "D" to r("1.0.0 <= v < 2.0.0")
+            ),
+            expect = mapOf(
+                    "B" to v(1, 0, 1),
+                    "C" to v(1, 0, 0),
+                    "D" to v(1, 0, 0)
+            ))
+
+    @Test
+    fun `no solution because version constraint is lower than lowest version in repo`() = doTest(
+            deps = mapOf("C" to r("0.0.0 <= v < 1.0.0")),
+            expect = null)
 
     @Test
     fun `no solution because version constraint is higher than max version in repo`() = doTest(
