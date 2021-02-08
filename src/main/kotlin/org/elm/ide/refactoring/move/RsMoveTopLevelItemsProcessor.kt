20fb7393c99fac83cd8ded5b00e9043ead90d26f
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2021-02-08 07:42:01 +0600

    [feature] basic importing of required members

--- src/main/kotlin/org/elm/ide/refactoring/move/RsMoveTopLevelItemsProcessor.kt
+++ src/main/kotlin/org/elm/ide/refactoring/move/RsMoveTopLevelItemsProcessor.kt
@@ -9,10 +9,11 @@ import com.intellij.refactoring.BaseRefactoringProcessor
 import com.intellij.refactoring.move.MoveMultipleElementsViewDescriptor
 import com.intellij.usageView.UsageInfo
 import com.intellij.usageView.UsageViewDescriptor
+import org.elm.lang.core.imports.ImportAdder
 import org.elm.lang.core.psi.ElmFile
 import org.elm.lang.core.psi.ElmPsiFactory
 import org.elm.lang.core.psi.elements.*
-import java.util.logging.Logger
+import org.elm.lang.core.psi.moduleName
 
 class RsMoveTopLevelItemsProcessor(
     private val project: Project,
@@ -28,6 +29,12 @@ class RsMoveTopLevelItemsProcessor(
     override fun findUsages(): Array<UsageInfo> {
         if (!searchForReferences) return UsageInfo.EMPTY_ARRAY
         return itemsToMove
+            .mapNotNull {
+                when (it) {
+                    is ElmValueDeclaration -> it.functionDeclarationLeft
+                    else -> it
+                }
+            }
             .flatMap { ReferencesSearch.search(it, GlobalSearchScope.projectScope(project)) }
             .filterNotNull()
             .map { UsageInfo(it) }
@@ -56,18 +63,55 @@ class RsMoveTopLevelItemsProcessor(
         val exposingList: ElmExposingList? = moduleDeclaration?.exposingList
 
         if (usages.isNotEmpty() && moduleDeclaration != null && !moduleDeclaration.exposesAll && exposingList != null) {
-            itemsToMove
+            val itemNames: List<String> = itemsToMove
                 .mapNotNull {
                     when (it) {
-                        is ElmValueDeclaration -> it.name
+                        is ElmValueDeclaration -> it.functionDeclarationLeft?.lowerCaseIdentifier?.text
                         is ElmTypeAliasDeclaration -> it.name
                         is ElmTypeDeclaration -> it.name + "(..)"
                         else -> null
                     }
                 }
-                .forEach(exposingList::addItem)
+
+            itemNames.forEach(exposingList::addItem)
+
+            usages
+                .mapNotNull { it.file as ElmFile? }
+                .distinct()
+                .forEach {
+                    var index: Int = getImportClauseIndex(it, moduleDeclaration)
+                    var fileAdded = false
+                    var names: List<String> = itemNames
+
+                    if (index == -1 && itemNames.isNotEmpty()) {
+                        ImportAdder.addImport(
+                            ImportAdder.Import(moduleDeclaration.moduleName, null, itemNames.first()),
+                            it,
+                            false
+                        )
+                        fileAdded = true
+                        index = getImportClauseIndex(it, moduleDeclaration)
+                    }
+
+                    if (!it.getImportClauses()[index].exposesAll) {
+                        if (fileAdded) {
+                            names = names.drop(1)
+                        }
+
+                        val importingList: ElmExposingList? = it.getImportClauses()[index].exposingList
+                        names.forEach { importingList?.addItem(it) }
+                    }
+                }
         }
     }
+
+    private fun getImportClauseIndex(
+        file: ElmFile,
+        moduleDeclaration: ElmModuleDeclaration
+    ) = file
+        .getImportClauses()
+        .map(ElmImportClause::referenceName)
+        .indexOf(moduleDeclaration.moduleName)
 
     override fun getCommandName(): String {
         return "Move Items"
