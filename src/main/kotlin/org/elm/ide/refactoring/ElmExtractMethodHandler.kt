d03e8a84bcdbb02bf99af0a9bd51bb43413c64d6
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2021-02-15 05:53:45 +0600

    [refactor] common refactor methods moved

--- src/main/kotlin/org/elm/ide/refactoring/ElmExtractMethodHandler.kt
+++ src/main/kotlin/org/elm/ide/refactoring/ElmExtractMethodHandler.kt
@@ -1,61 +1,14 @@
 package org.elm.ide.refactoring
 
-import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.refactoring.RefactoringActionHandler
-import com.intellij.refactoring.RefactoringBundle
-import com.intellij.refactoring.util.CommonRefactoringUtil
-import org.elm.ide.utils.findExpressionAtCaret
-import org.elm.ide.utils.findExpressionInRange
 import org.elm.lang.core.psi.*
 import org.elm.lang.core.psi.elements.ElmFunctionDeclarationLeft
-import org.elm.lang.core.psi.elements.ElmLetInExpr
 import org.elm.lang.core.psi.elements.ElmValueDeclaration
 import org.elm.openapiext.runWriteCommandAction
 
-class ElmExtractMethodHandler : RefactoringActionHandler {
+class ElmExtractMethodHandler : ElmExtractorBase() {
 
-    override fun invoke(project: Project, editor: Editor, file: PsiFile, dataContext: DataContext?) {
-        if (file !is ElmFile) return
-        val exprs = findCandidateExpressions(editor, file)
-        when (exprs.size) {
-            0 -> {
-                val message = RefactoringBundle.message(
-                    if (editor.selectionModel.hasSelection())
-                        "selected.block.should.represent.an.expression"
-                    else
-                        "refactoring.introduce.selection.error"
-                )
-                val title = RefactoringBundle.message("introduce.variable.title")
-                val helpId = "refactoring.extractVariable"
-                CommonRefactoringUtil.showErrorHint(project, editor, message, title, helpId)
-            }
-            1 -> introduceVariable(editor, exprs.single())
-            else -> showExpressionChooser(editor, exprs) { chosenExpr ->
-                introduceVariable(editor, chosenExpr)
-            }
-        }
-    }
-
-    private fun findCandidateExpressions(editor: Editor, file: ElmFile): List<ElmExpressionTag> {
-        val selection = editor.selectionModel
-        return if (selection.hasSelection()) {
-            // If the user has some text selected, make a single suggestion based on the selection
-            listOfNotNull(findExpressionInRange(file, selection.selectionStart, selection.selectionEnd))
-        } else {
-            // Suggest nested expressions at caret position
-            val expr = findExpressionAtCaret(file, editor.caretModel.offset) ?: return emptyList()
-            expr.ancestors
-                .takeWhile { it !is ElmValueDeclaration && it !is ElmLetInExpr }
-                .filterIsInstance<ElmExpressionTag>()
-                .toList()
-        }
-    }
-
-    private fun introduceVariable(editor: Editor, chosenExpr: ElmExpressionTag) {
+    override fun process(editor: Editor, chosenExpr: ElmExpressionTag) {
         if (!chosenExpr.isValid) return
         val project = editor.project ?: return
 
@@ -94,9 +47,5 @@ class ElmExtractMethodHandler : RefactoringActionHandler {
         }
 
         return null
-    }
-
-    override fun invoke(project: Project, elements: Array<out PsiElement>, dataContext: DataContext?) {
-        // IntelliJ will never call this when introducing a variable
     }
 }
--- src/main/kotlin/org/elm/ide/refactoring/ElmIntroduceVariableHandler.kt
+++ src/main/kotlin/org/elm/ide/refactoring/ElmIntroduceVariableHandler.kt
@@ -1,20 +1,12 @@
 package org.elm.ide.refactoring
 
-import com.intellij.application.options.CodeStyle
-import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiNamedElement
-import com.intellij.refactoring.RefactoringActionHandler
-import com.intellij.refactoring.RefactoringBundle
 import com.intellij.refactoring.introduce.inplace.InplaceVariableIntroducer
-import com.intellij.refactoring.util.CommonRefactoringUtil
 import com.intellij.util.DocumentUtil
-import org.elm.ide.utils.findExpressionAtCaret
-import org.elm.ide.utils.findExpressionInRange
 import org.elm.lang.core.psi.*
 import org.elm.lang.core.psi.elements.ElmAnonymousFunctionExpr
 import org.elm.lang.core.psi.elements.ElmCaseOfBranch
@@ -23,45 +15,9 @@ import org.elm.lang.core.psi.elements.ElmValueDeclaration
 import org.elm.lang.core.textWithNormalizedIndents
 import org.elm.openapiext.runWriteCommandAction
 
-class ElmIntroduceVariableHandler : RefactoringActionHandler {
+class ElmIntroduceVariableHandler : ElmExtractorBase() {
 
-    override fun invoke(project: Project, editor: Editor, file: PsiFile, dataContext: DataContext?) {
-        if (file !is ElmFile) return
-        val exprs = findCandidateExpressions(editor, file)
-        when (exprs.size) {
-            0 -> {
-                val message = RefactoringBundle.message(if (editor.selectionModel.hasSelection())
-                    "selected.block.should.represent.an.expression"
-                else
-                    "refactoring.introduce.selection.error"
-                )
-                val title = RefactoringBundle.message("introduce.variable.title")
-                val helpId = "refactoring.extractVariable"
-                CommonRefactoringUtil.showErrorHint(project, editor, message, title, helpId)
-            }
-            1 -> introduceVariable(editor, exprs.single())
-            else -> showExpressionChooser(editor, exprs) { chosenExpr ->
-                introduceVariable(editor, chosenExpr)
-            }
-        }
-    }
-
-    private fun findCandidateExpressions(editor: Editor, file: ElmFile): List<ElmExpressionTag> {
-        val selection = editor.selectionModel
-        return if (selection.hasSelection()) {
-            // If the user has some text selected, make a single suggestion based on the selection
-            listOfNotNull(findExpressionInRange(file, selection.selectionStart, selection.selectionEnd))
-        } else {
-            // Suggest nested expressions at caret position
-            val expr = findExpressionAtCaret(file, editor.caretModel.offset) ?: return emptyList()
-            expr.ancestors
-                    .takeWhile { it !is ElmValueDeclaration && it !is ElmLetInExpr }
-                    .filterIsInstance<ElmExpressionTag>()
-                    .toList()
-        }
-    }
-
-    private fun introduceVariable(editor: Editor, chosenExpr: ElmExpressionTag) {
+    override fun process(editor: Editor, chosenExpr: ElmExpressionTag) {
         if (!chosenExpr.isValid) return
         val project = editor.project ?: return
 
@@ -88,10 +44,6 @@ class ElmIntroduceVariableHandler : RefactoringActionHandler {
             current = current.parent
         }
         return null
-    }
-
-    override fun invoke(project: Project, elements: Array<out PsiElement>, dataContext: DataContext?) {
-        // IntelliJ will never call this when introducing a variable
     }
 }
 
@@ -182,22 +134,10 @@ private class ExpressionReplacer(
     }
 }
 
-
-class ElmInplaceVariableIntroducer(
-        elementToRename: PsiNamedElement,
-        editor: Editor,
-        project: Project,
-        title: String,
-        occurrences: Array<PsiElement>
+private class ElmInplaceVariableIntroducer(
+    elementToRename: PsiNamedElement,
+    editor: Editor,
+    project: Project,
+    title: String,
+    occurrences: Array<PsiElement>
 ) : InplaceVariableIntroducer<PsiElement>(elementToRename, editor, project, title, occurrences, null)
-
-
-fun moveEditorToNameElement(editor: Editor, element: PsiElement?): PsiNamedElement? {
-    val newName = element?.descendants
-            ?.filterIsInstance<ElmNameIdentifierOwner>()
-            ?.firstOrNull { it.nameIdentifier.elementType == ElmTypes.LOWER_CASE_IDENTIFIER }
-    if (newName != null) {
-        editor.caretModel.moveToOffset(newName.nameIdentifier.startOffset)
-    }
-    return newName
-}
