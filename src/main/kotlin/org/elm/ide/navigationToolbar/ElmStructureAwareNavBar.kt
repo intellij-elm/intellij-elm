163f450f60e5be44ac21e1182c40261187a68524
pravdomil <pravdomil.toman@gmail.com>
2021-03-31 18:58:22 +0200

    Update ElmStructureAwareNavBar.kt

--- src/main/kotlin/org/elm/ide/navigationToolbar/ElmStructureAwareNavBar.kt
+++ src/main/kotlin/org/elm/ide/navigationToolbar/ElmStructureAwareNavBar.kt
@@ -2,6 +2,6 @@ package org.elm.ide.navigationToolbar
 
 import com.intellij.ide.navigationToolbar.StructureAwareNavBarModelExtension
 
-abstract class ElmStructureAwareNavBar : StructureAwareNavBarModelExtension() {
-  // What do here?
+class ElmStructureAwareNavBar : StructureAwareNavBarModelExtension() {
+  // What to do here?
 }
